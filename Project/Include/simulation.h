983d8fe0fa82dfaf40b12644a2c1721e076dad82
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-06 17:23:30 +0100

    Documentation

--- Project/Include/simulation.h
+++ Project/Include/simulation.h
@@ -1,39 +1,48 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
 
+#include "initFunc.h"
 #include "simData.h"
+#include "model.h"
 
+//! The Simulation interface for the programme
+/*!
+    The Simulation object is the furthest abstracted class of the project. All
+  modules will be held in here, and they should have no knowledge of this class.
+  The constructor takes a simData object (such that in this constructor we can
+  allocate memory for the state vectors). All other system objects are set with
+  the corresponding set functions.
+*/
 class Simulation
 {
 
   private:
+
+    //! Initial function object to set up starting data
+    InitialFunc * initFuncObj;
+
+    //! Form of simulation, governing equations and spectral analysis
+    Model * modelObj;
+
+    //! Incrememt the system forward by a single timestep
     void updateTime();
 
+    //! Implements the initial set up of prim vars
+    void initialize();
+
   public:
-            /*~~~~~~~~~~~~~~~    Public access data ~~~~~~~~~~~~*/
+
+    //! simData class containing all necessary variables
     Data * data;
 
-    /* Simulation set up */
-    // Model * model;
-    // Will need to store bcs, initFunc, timeInt, source etcetera
-
-
-          /*~~~~~~~~~~~~~~~    Public member functions ~~~~~~~~~~~~*/
-    /*
-      Constructor: Sets the constants for the simulation
-    */
-
+    //! Constructor sets data and allocates memory
     Simulation(Data * data);
 
+    //! Destructor frees alloc'd memory
     ~Simulation();
-    /*
-      initialize: Sets the grid, model, initial data, time integrator, source terms
-        and any other required
-    */
-    // void initialize(Model * model);
-    /*
-      evolve: Runs the initialized simulation until endTime.
-    */
+
+
+    //!
     void evolve();
 
 
