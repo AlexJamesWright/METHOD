a10373d08a3c718afe8e7116cf9a225d49e85f1f
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-06 17:20:52 +0100

    Playing with initial function

--- Project/Src/main.cu
+++ Project/Src/main.cu
@@ -1,16 +1,23 @@
 #include "simData.h"
 #include "simulation.h"
+#include "initFunc.h"
 #include "model.h"
 #include <iostream>
 
 
 int main(void) {
 
-  Data data(10, 10, 0.0, 1.0, 0.0, 1.0, 1.0);
-  SRMHD model(&data);
+  // Set up domain
+  Data data(10, 10, 0.0, 1.0, 0.0, 1.0, 0.4);
+  // Choose particulars of simulation
+  SRMHD Model(&data);
+  OTVortex Init(&data);
+
+
+
+
   Simulation sim(&data);
 
-  std::cout << "Ncons = " << sim.data->Ncons << std::endl;
 
   return 0;
 
