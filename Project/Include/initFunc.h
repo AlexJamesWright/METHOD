6ca343986d538b11146a319df8df846e453299e1
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-06 17:22:21 +0100

    Initial functions derive from initialFunc base class

--- Project/Include/initFunc.h
+++ Project/Include/initFunc.h
@@ -1,18 +1,37 @@
 #ifndef INITFUNC_H
 #define INITFUNC_H
 
-#include "simulation.h"
+#include "simData.h"
 
+//! Base Class implements the initial data.
+/*!
+    All initial set ups are contained within the InitialFunc object. The class
+  is initialised with just the simData class, and then one of the member functions
+  is selected to be applied.
+*/
 class InitialFunc
 {
   private:
-    *Simulation sim;
+
+    //! simData class containing all necessary variables
+    Data * data;
 
   public:
-    InitialFunc(*Simulation);
-    **double OTVortex(void);
+
+    //! Constructor stores the location of the simData
+    InitialFunc(Data * data);
+};
 
 
+//! Orszag-Tang voretx initial data
+/*!
+    See Orszag and Tang 1979, 'Small scale structure of two dimensional...'
+  or visit http://flash.uchicago.edu/site/flashcode/user_support/flash_ug_devel/node178.html
+*/
+class OTVortex : public InitialFunc
+{
+  public:
+    OTVortex(Data * data);
 };
 
 
