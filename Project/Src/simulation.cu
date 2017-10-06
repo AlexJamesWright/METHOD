960dc54757370cb4042786520ba64f285a2c9d1a
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-06 17:21:40 +0100

    Constructor allocates memory and sets rest of data where available

--- Project/Src/simulation.cu
+++ Project/Src/simulation.cu
@@ -3,30 +3,57 @@
 
 Simulation::Simulation(Data * data) : data(data)
 {
-  // Allocate memory for state arrays
-  int Ntot(this->data->Nx * this->data->Ny);
+  // Simplify syntax
+  Data * d;
+  d = this->data;
 
-  gpuErrchk( cudaHostAlloc((void **)&this->data->cons,
-                sizeof(double) * Ntot * this->data->Ncons,
+  // Allocate memory for state arrays
+  int Ntot(d->Nx * d->Ny);
+
+  gpuErrchk( cudaHostAlloc((void **)&d->cons,
+                sizeof(double) * Ntot * d->Ncons,
                 cudaHostAllocPortable) );
-  gpuErrchk( cudaHostAlloc((void **)&this->data->f,
-                sizeof(double) * Ntot * this->data->Ncons,
+  gpuErrchk( cudaHostAlloc((void **)&d->f,
+                sizeof(double) * Ntot * d->Ncons,
                 cudaHostAllocPortable) );
-  gpuErrchk( cudaHostAlloc((void **)&this->data->fnet,
-                sizeof(double) * Ntot * this->data->Ncons,
+  gpuErrchk( cudaHostAlloc((void **)&d->fnet,
+                sizeof(double) * Ntot * d->Ncons,
                 cudaHostAllocPortable) );
-  gpuErrchk( cudaHostAlloc((void **)&this->data->source,
-                sizeof(double) * Ntot * this->data->Ncons,
+  gpuErrchk( cudaHostAlloc((void **)&d->source,
+                sizeof(double) * Ntot * d->Ncons,
                 cudaHostAllocPortable) );
-  gpuErrchk( cudaHostAlloc((void **)&this->data->prims,
-                sizeof(double) * Ntot * this->data->Nprims,
+  gpuErrchk( cudaHostAlloc((void **)&d->prims,
+                sizeof(double) * Ntot * d->Nprims,
                 cudaHostAllocPortable) );
-  gpuErrchk( cudaHostAlloc((void **)&this->data->aux,
-                sizeof(double) * Ntot * this->data->Naux,
+  gpuErrchk( cudaHostAlloc((void **)&d->aux,
+                sizeof(double) * Ntot * d->Naux,
                 cudaHostAllocPortable) );
-  gpuErrchk( cudaHostAlloc((void **)&this->data->x,
-                sizeof(double) * Ntot,
+  gpuErrchk( cudaHostAlloc((void **)&d->x,
+                sizeof(double) * d->Nx,
                 cudaHostAllocPortable) );
+  gpuErrchk( cudaHostAlloc((void **)&d->y,
+                sizeof(double) * d->Ny,
+                cudaHostAllocPortable) );
+
+
+  // Initialise the data
+  d->dx = (d->xmax - d->xmin) / d->Nx;
+  d->dy = (d->ymax - d->ymin) / d->Ny;
+  d->iters = 0;
+  d->t = 0;
+  d->alphaX = 1;
+  d->alphaY = 1;
+  d->dt = d->cfl * (d->alphaX / d->dx + d->alphaY / d->dy);
+
+  // Set axes
+  for (int i(0); i < d->Nx; i++) {
+    d->x[i] = d->xmin + (i + 0.5 - d->Ng) * d->dx;
+  }
+  for (int j(0); j < d->Ny; j++) {
+    d->y[j] = d->ymin + (j + 0.5 - d->Ng) * d->dy;
+  }
+
+
 }
 
 Simulation::~Simulation()
