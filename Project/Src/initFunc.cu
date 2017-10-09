e0ea79a2c06e112264672077c09ce59e35238162
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-09 09:56:07 +0100

    Base constructor and OTVortex implemented

--- Project/Src/initFunc.cu
+++ Project/Src/initFunc.cu
@@ -1,29 +1,61 @@
 #include "initFunc.h"
-#include <iostream>
+#include <stdexcept>
+#include <cmath>
 
 InitialFunc::InitialFunc(Data * data) : data(data)
 {
   Data * d;
   d = this->data;
 
-  std::cout << "Element (4, 6, 6) = " << d->cons[4*d->Ny*d->Nx + 6*d->Ny + 6] << std::endl;
-
-  std::cout << "Setting cons data..." << std::endl;
+  // Ensure that the memory has been allocated for the arrays
+  if (!d->dataSet) throw std::runtime_error("Must construct simulation class before implementing initial state. Need to allocate arrays.");
 
   // Set all state vectors to zero
   for (int i(0); i < d->Nx; i++) {
+    // Cons, source, fluxes
     for (int j(0); j < d->Ny; j++) {
       for (int var(0); var < d->Ncons; var++) {
-        d->cons[(var, i, j)] = j;
+        d->cons[d->id(var, i, j)] = 0;
+        d->f[d->id(var, i, j)] = 0;
+        d->fnet[d->id(var, i, j)] = 0;
+        d->source[d->id(var, i, j)] = 0;
+      }
+      // Primitives
+      for (int var(0); var < d->Nprims; var++) {
+        d->prims[d->id(var, i, j)] = 0;
+      }
+      // Auxalliary
+      for (int var(0); var < d->Naux; var++) {
+        d->aux[d->id(var, i, j)] = 0;
       }
     }
   }
 
-  std::cout << "Element (4, 6, 6) = " << d->cons[4*d->Ny*d->Nx + 6*d->Ny + 6] << std::endl;
 
 }
 
 OTVortex::OTVortex(Data * data) : InitialFunc(data)
 {
+  const double pi(3.141592653589793238462);
+
+  Data * d;
+  d = data;
+
+  for (int i(0); i < d->Nx; i++) {
+    for (int j(0); j < d->Ny; j++) {
+      // Density and pressure
+      d->prims[d->id(0, i, j)] = 25 / 36 / pi;
+      d->prims[d->id(4, i, j)] = 5 / 12 / pi;
+
+      // x-velocity and x-Bfield
+      d->prims[d->id(1, i, j)] = - 0.5 * sin(2 * pi * d->y[j]);
+      d->prims[d->id(5, i, j)] = - sin(2 * pi * d->y[j]) / sqrt(4 * pi);
+
+      // y-velocity and y-Bfield
+      d->prims[d->id(2, i, j)] = 0.5 * sin(2 * pi * d->x[i]);
+      d->prims[d->id(6, i, j)] = sin(4 * pi * d->x[i]) / sqrt(4 * pi);
+    }
+  }
+
 
 }
