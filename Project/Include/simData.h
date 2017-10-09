f7f33acf4a5b93777538fdd790667872b7c7eb7a
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-09 11:29:04 +0100

    Tidied up and documentation

--- Project/Include/simData.h
+++ Project/Include/simData.h
@@ -11,45 +11,60 @@ class Data
 {
   public:
     int
-    Nx, Ny; // Total number of cells in x- and y-direction
+    Nx, Ny;               // Total number of cells in x- and y-direction
     double
-    xmin, xmax, // x-axis limits
-    ymin, ymax, // y-axis limits
-    endTime,   // End time of simulation
-    cfl;  // Courant factor
-    int Ng; // Number of ghost cells
+    xmin, xmax,           // x-axis limits
+    ymin, ymax,           // y-axis limits
+    endTime,              // End time of simulation
+    cfl;                  // Courant factor
+    int Ng;               // Number of ghost cells
     double
-    gamma,  // Adiabatic index
-    sigma; // Resistivity
+    gamma,                // Adiabatic index
+    sigma;                // Resistivity
     int
-    dataSet, // Indicator that memory has been allocated for state vectors
-    Ncons, Nprims, Naux; // Number of conserved, primitive and auxilliary variables
+    dataSet,              // Indicator that memory has been allocated for state vectors
+    Ncons, Nprims, Naux;  // Number of conserved, primitive and auxilliary variables
     double
-    *cons, *prims, *aux, *f, *fnet, *source, *x, *y; // State vectors, flux and numerical flux, source vector and grid points (center)
-    double alphaX, alphaY, t, dt, dx, dy;
+    *cons, *prims, *aux,  // State vectors of conserved, primitive and auxilliary vars
+    *f, *fnet,            // Flux vector and net numerical flux vector
+    *source,              // Source vector
+    *x, *y;               // x- and y-coordinate positions (incl ghost cells)
+    double alphaX, alphaY,// Max wave speed in x and y direction
+    t, dt,                // Current time and timestep
+    dx, dy;               // Spatial x and y step
     int
-    iters; // Number of interations that have been completed
+    iters;                // Number of interations that have been completed
 
 
-    //! Overload the () operator for accessing array elements
+    //! Element ID function
     /*!
         To access the 2nd conserved variable at (x, y) = (12, 4) for example,
-      we call data.cons[(2, 12, 4)].
+      we call elem=data.id(2, 12, 4) and use this in d.cons[elem].
     */
     int id(int var, int i, int j) {
       return var * this->Nx * this->Ny + i * this->Ny + j;
     }
 
+    //! Constructor
+    /*!
+        Allocates the memory required for the state arrays and sets the simulation
+      constants to the given values. Does not set initial state, thats done by
+      the initialFunc object.
+    */
     Data(int Nx, int Ny,
          double xmin, double xmax,
          double ymin, double ymax,
          double endTime, double cfl=0.5, int Ng=4,
-         double gamma=5.0/3.0, double sigma=0, int dataSet=0) :
+         double gamma=5.0/3.0, double sigma=0,
+         int dataSet=0,
+         int Ncons=0, int Nprims=0, int Naux=0) :
          Nx(Nx), Ny(Ny),
          xmin(xmin), xmax(xmax),
          ymin(ymin), ymax(ymax),
          endTime(endTime), cfl(cfl), Ng(Ng),
-         gamma(gamma), sigma(sigma), dataSet(dataSet) { }
+         gamma(gamma), sigma(sigma),
+         dataSet(dataSet),
+         Ncons(Ncons), Nprims(Nprims), Naux(Naux) { }
 
 };
 
