74914da99c8d6680bfa5bc0f496e663960f5888c
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-09 12:27:23 +0100

    Constructor take number of physical cells nx, ny and stores the TOTAL number Nx,Ny

--- Project/Src/simData.cu
+++ Project/Src/simData.cu
@@ -1,1 +1,20 @@
 #include "simData.h"
+
+Data::Data(int nx, int ny,
+     double xmin, double xmax,
+     double ymin, double ymax,
+     double endTime, double cfl, int Ng,
+     double gamma, double sigma,
+     int dataSet,
+     int Ncons, int Nprims, int Naux) :
+     nx(nx), ny(ny),
+     xmin(xmin), xmax(xmax),
+     ymin(ymin), ymax(ymax),
+     endTime(endTime), cfl(cfl), Ng(Ng),
+     gamma(gamma), sigma(sigma),
+     dataSet(dataSet),
+     Ncons(Ncons), Nprims(Nprims), Naux(Naux)
+{
+  this->Nx = nx + 2 * Ng;
+  this->Ny = ny + 2 * Ng;
+}
