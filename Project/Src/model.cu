2262ea12d51b6a92d00bed902c862aae4600b80c
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-09 14:02:19 +0100

    Started the prims2all conversion

--- Project/Src/model.cu
+++ Project/Src/model.cu
@@ -4,14 +4,14 @@ SRMHD::SRMHD() : Model()
 {
   this->Ncons = 9;
   this->Nprims = 8;
-  this->Naux = 9;
+  this->Naux = 10;
 }
 
 SRMHD::SRMHD(Data * data) : Model(data)
 {
   this->Ncons = (this->data)->Ncons = 9;
   this->Nprims = (this->data)->Nprims = 8;
-  this->Naux = (this->data)->Naux = 9;
+  this->Naux = (this->data)->Naux = 10;
 }
 
 
@@ -25,7 +25,17 @@ void SRMHD::getPrimitiveVars(double *cons, double *prims, double *aux)
 
 }
 
-void SRMHD::consToPrims(double *cons, double *prims, double *aux)
+void SRMHD::primsToAll(double *cons, double *prims, double *aux)
 {
+  // Syntax
+  Data * d = this->data;
 
+  for (int i(0); i < d->Nx; i++) {
+    for (int j(0); j < d->Ny; j++) {
+      // vsq
+      d->aux[d->id(9, i, j)] = d->prims[d->id(1, i, j)] * d->prims[d->id(1, i, j)] +
+                               d->prims[d->id(2, i, j)] * d->prims[d->id(2, i, j)] +
+                                d->prims[d->id(3, i, j)] * d->prims[d->id(3, i, j)];
+    }
+  }
 }
