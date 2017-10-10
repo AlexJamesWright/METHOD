1a248826456849703ce4fcceafb4e7ad2571aa91
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-10 15:53:38 +0100

    computes flux vector

--- Project/Src/srmhd.cu
+++ Project/Src/srmhd.cu
@@ -1,8 +1,9 @@
 #include "srmhd.h"
+#include "weno.h"
 #include <cmath>
 
 SRMHD::SRMHD() : Model()
-{
+{ 
   this->Ncons = 9;
   this->Nprims = 8;
   this->Naux = 10;
@@ -16,10 +17,109 @@ SRMHD::SRMHD(Data * data) : Model(data)
 }
 
 
+//! Generates the net numerical flux given the current state
+/*!
+    We are using the flux vector splitting method described in Shu, `Essentially
+  Non-Oscillatory and Weighted Essentially Non-Oscillatory Schemes for Hyperbolic
+  Conservation Laws`. For the form of the fluxes see Relativistic Magneto..., Anton '10
+  with the inclusion of divergence cleaning from Advanced numerical methods for Neutron star
+  interfaces, John Muddle.
+    Note: We are assuming that all primitive and auxilliary variables are up-to-date
+  at the time of this function execution.
+*/
 void SRMHD::fluxFunc(double *cons, double *prims, double *aux, double *f, double *fnet, int dir)
 {
+  // Syntax
+  Data * d(this->data);
+
+  // Order of weno scheme
+  int order(2);
+  
+  // Generate flux vector
+  for (int i(0); i < d->Nx; i++) {
+    for (int j(0); j < d->Ny; j++) {
+
+      // Fx: flux in x-direction
+      if (dir == 0) {
+        // D
+        f[d->id(0, i, j)] = cons[d->id(0, i, j)] * prims[d->id(1, i, j)];
+
+        // Sx
+        f[d->id(1, i, j)] = cons[d->id(1, i, j)] * prims[d->id(1, i, j)] +
+                               prims[d->id(4, i, j)] + aux[d->id(8, i, j)] / 2.0 -
+                               aux[d->id(5, i, j)] * prims[d->id(5, i, j)] /
+                               aux[d->id(1, i, j)];
+        // Sy
+        f[d->id(2, i, j)] = cons[d->id(2, i, j)] * prims[d->id(1, i, j)] -
+                               aux[d->id(6, i, j)] * prims[d->id(5, i, j)] / 
+                               aux[d->id(1, i, j)];
+        // Sz
+        f[d->id(3, i, j)] = cons[d->id(3, i, j)] * prims[d->id(1, i, j)] -
+                               aux[d->id(7, i, j)] * prims[d->id(5, i, j)] /
+                               aux[d->id(1, i, j)];
+        // tau
+        f[d->id(4, i, j)] = (cons[d->id(4, i, j)] + prims[d->id(4, i, j)] +
+                               aux[d->id(8, i, j)] / 2.0) * prims[d->id(1, i, j)] -
+                               aux[d->id(4, i, j)] * prims[d->id(5, i, j)] /
+                               aux[d->id(1, i, j)];
+        // Bx
+        f[d->id(5, i, j)] = cons[d->id(8, i, j)];
+
+        // By
+        f[d->id(6, i, j)] = prims[d->id(6, i, j)] * prims[d->id(1, i, j)] -
+                               prims[d->id(5, i, j)] * prims[d->id(2, i, j)];
+        // Bz
+        f[d->id(7, i, j)] = prims[d->id(7, i, j)] * prims[d->id(1, i, j)] - 
+                               prims[d->id(5, i, j)] * prims[d->id(3, i, j)];
+        // Phi
+        f[d->id(8, i, j)] = prims[d->id(5, i, j)];
+      }
+
+      // Fy: flux in y-direction
+      if (dir == 1) {
+        // D
+        f[d->id(0, i, j)] = cons[d->id(0, i, j)] * prims[d->id(2, i, j)];
+
+        // Sx
+        f[d->id(1, i, j)] = cons[d->id(1, i, j)] * prims[d->id(2, i, j)] -
+                            aux[d->id(5, i, j)] * prims[d->id(6, i, j)] /
+                            aux[d->id(1, i, j)];
+        // Sy
+        f[d->id(2, i, j)] = cons[d->id(2, i, j)] * prims[d->id(2, i, j)] +
+                            prims[d->id(4, i, j)] + aux[d->id(9, i, j)] / 2.0 -
+                            aux[d->id(6, i, j)] * prims[d->id(6, i, j)] /
+                            aux[d->id(1, i, j)];
+        // Sz
+        f[d->id(3, i, j)] = cons[d->id(3, i, j)] * prims[d->id(2, i, j)] -
+                            aux[d->id(7, i, j)] * prims[d->id(6, i, j)] /
+                            aux[d->id(1, i, j)];
+        // tau
+        f[d->id(4, i, j)] = (cons[d->id(4, i, j)] + prims[d->id(4, i, j)] +
+                            aux[d->id(8, i, j)]) * prims[d->id(2, i, j)] -
+                            aux[d->id(4, i, j)] * prims[d->id(6, i, j)] /
+                            aux[d->id(1, i, j)];
+        // Bx
+        f[d->id(5, i, j)] = prims[d->id(5, i, j)] * prims[d->id(2, i, j)] -
+                            prims[d->id(6, i, j)] * prims[d->id(1, i, j)];
+        // By
+        f[d->id(6, i, j)] = cons[d->id(8, i, j)];
+        
+        // Bz
+        f[d->id(7, i, j)] = prims[d->id(7, i, j)] * prims[d->id(2, i, j)] -
+                            prims[d->id(6, i, j)] * prims[d->id(3, i, j)]
+        // Phi
+        f[d->id(8, i, j)] = prims[d->id(6, i, j)];
+      }
+
+
+
+
+    }
+  }
+
 
 }
+
 
 //! Source required for divergence cleaning
 /*!
