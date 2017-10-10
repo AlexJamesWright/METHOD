2a47bedb94bd83149f73d0ef62814f2ff2e9fc9c
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-10 12:58:46 +0100

    parameter for divergence cleaning

--- Project/Src/simData.cu
+++ Project/Src/simData.cu
@@ -6,14 +6,16 @@ Data::Data(int nx, int ny,
      double endTime, double cfl, int Ng,
      double gamma, double sigma,
      int dataSet,
-     int Ncons, int Nprims, int Naux) :
+     int Ncons, int Nprims, int Naux,
+     double cp) :
      nx(nx), ny(ny),
      xmin(xmin), xmax(xmax),
      ymin(ymin), ymax(ymax),
      endTime(endTime), cfl(cfl), Ng(Ng),
      gamma(gamma), sigma(sigma),
      dataSet(dataSet),
-     Ncons(Ncons), Nprims(Nprims), Naux(Naux)
+     Ncons(Ncons), Nprims(Nprims), Naux(Naux),
+     cp(cp)
 {
   this->Nx = nx + 2 * Ng;
   this->Ny = ny + 2 * Ng;
