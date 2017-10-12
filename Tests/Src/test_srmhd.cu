b26218236f5e89b4ef68be17aa59b4b365b140e8
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-12 14:26:26 +0100

    Tests getPrimVars is working: OTVortex to cons back to prims, and otvortex to cons, change prims a little and still solve for same cons

--- Tests/Src/test_srmhd.cu
+++ Tests/Src/test_srmhd.cu
@@ -5,8 +5,10 @@
 #include "initFunc.h"
 #include <cstdlib>
 #include <cmath>
+#include <stdio.h>
 
-namespace {
+namespace
+{
 
   /* ##################### Test default model constructor ####################*/
 
@@ -15,7 +17,7 @@ namespace {
     SRMHD model;
     EXPECT_EQ(model.Ncons, 9);
     EXPECT_EQ(model.Nprims, 8);
-    EXPECT_EQ(model.Naux, 10);
+    EXPECT_EQ(model.Naux, 13);
   }
 
 
@@ -29,9 +31,166 @@ namespace {
     SRMHD model(&d);
     EXPECT_EQ(model.data->Ncons, 9);
     EXPECT_EQ(model.data->Nprims, 8);
-    EXPECT_EQ(model.data->Naux, 10);
+    EXPECT_EQ(model.data->Naux, 13);
 
   }
+
+
+
+  /* ######################### Test flux vector splitting ########################*/
+
+  TEST(SRMHD, FluxVectorSplittingStationary)
+  {
+
+    // Set up
+    Data d(10, 10, 0, 1, 0, 1, 1.0, 0.5, 4, 5.0/3.0, 0.0, 0, 0, 0, 0, 0.5);
+    SRMHD model(&d);
+    Simulation sim(&d);
+
+    // Set state to stationary equilibrium state
+    for (int i(0); i < d.Nx; i++) {
+      for (int j(0); j < d.Ny; j++) {
+        d.prims[d.id(0, i, j)] = 0.5; // Require non-zero density
+        d.prims[d.id(1, i, j)] = 0.0;
+        d.prims[d.id(2, i, j)] = 0.0;
+        d.prims[d.id(3, i, j)] = 0.0;
+        d.prims[d.id(4, i, j)] = 0.0;
+        d.prims[d.id(5, i, j)] = 0.0;
+        d.prims[d.id(6, i, j)] = 0.0;
+        d.prims[d.id(7, i, j)] = 0.0;
+        d.prims[d.id(8, i, j)] = 0.0;
+      }
+    }
+
+    model.primsToAll(d.cons, d.prims, d.aux);
+
+    // System is stationary, there should be zero flux
+    // x-direction
+    model.fluxFunc(d.cons, d.prims, d.aux, d.f, d.fnet, 0);
+    for (int i(0); i < d.Nx; i++) {
+      for (int j(0); j < d.Ny; j++) {
+        for (int var(0); var < d.Ncons; var++) {
+          EXPECT_EQ(d.fnet[d.id(var, i, j)], 0.0);
+        }
+      }
+    }
+    // y-direction
+    model.fluxFunc(d.cons, d.prims, d.aux, d.f, d.fnet, 1);
+    for (int i(0); i < d.Nx; i++) {
+      for (int j(0); j < d.Ny; j++) {
+        for (int var(0); var < d.Ncons; var++) {
+          EXPECT_EQ(d.fnet[d.id(var, i, j)], 0.0);
+        }
+      }
+    }
+  }
+
+
+  /* ######################### Test source contribution ########################*/
+
+  TEST(SRMHD, SourceTerm)
+  {
+
+    // Set up
+    Data d(10, 10, 0, 1, 0, 1, 1.0, 0.5, 4, 5.0/3.0, 0.0, 0, 0, 0, 0, 0.5);
+    SRMHD model(&d);
+    Simulation sim(&d);
+
+    // Set cons to something
+    for (int i(0); i < d.Nx; i++) {
+      for (int j(0); j < d.Ny; j++) {
+        for (int var(0); var < d.Ncons; var++) {
+          d.cons[d.id(var, i, j)] = 3.1415926;
+        }
+      }
+    }
+    // Determine source
+    model.sourceTerm(d.cons, d.prims, d.aux, d.source);
+    for (int i(0); i < d.Nx; i++) {
+      for (int j(0); j < d.Ny; j++) {
+        for (int var(0); var < d.Ncons; var++) {
+          if (var == 8) EXPECT_EQ(d.source[d.id(var, i, j)], -3.1415926 / (0.25));
+          else EXPECT_EQ(d.source[d.id(var, i, j)], 0);
+        }
+      }
+    }
+  }
+
+
+
+  /* ######################### Test getPrimVars transform ########################*/
+
+  TEST(SRMHD, conservedToPrimitiveTransformation)
+  {
+    const double tol = 1.49011612e-8;   // Tolerance of rootfinder
+    Data d(10, 10, 0, 1, 0, 1, 1.0);
+    Data d2(10, 10, 0, 1, 0, 1, 1.0);
+    SRMHD model(&d);
+    SRMHD model2(&d2);
+    Simulation sim(&d);
+    Simulation sim2(&d2);
+    OTVortex init(&d);
+    OTVortex init2(&d2);
+
+    model2.primsToAll(d2.cons, d2.prims, d2.aux);
+    model.primsToAll(d2.cons, d2.prims, d2.aux);
+
+
+
+    model2.getPrimitiveVars(d2.cons, d2.prims, d2.aux);
+
+
+    for (int var(0); var < d.Nprims; var++) {
+      for (int i(0); i < d.Nx; i++) {
+        for (int j(0); j < d.Ny; j++) {
+          EXPECT_NEAR(d.prims[d.id(var, i, j)], d2.prims[d.id(var, i, j)], tol);
+        }
+      }
+    }
+    for (int var(0); var < d.Naux; var++) {
+      for (int i(0); i < d.Nx; i++) {
+        for (int j(0); j < d.Ny; j++) {
+          EXPECT_NEAR(d.aux[d.id(var, i, j)], d2.aux[d.id(var, i, j)], tol);
+        }
+      }
+    }
+
+    // Set all d2 prims slightly off so rootfind has to do something
+    for (int var(0); var < d.Nprims; var++) {
+      for (int i(0); i < d.Nx; i++) {
+        for (int j(0); j < d.Ny; j++) {
+          d2.prims[d.id(var, i, j)] *= 0.9;
+        }
+      }
+    }
+    for (int var(0); var < d.Naux; var++) {
+      for (int i(0); i < d.Nx; i++) {
+        for (int j(0); j < d.Ny; j++) {
+          d2.aux[d.id(var, i, j)] *= 0.9;
+        }
+      }
+    }
+    // Solve and re-check
+    model2.getPrimitiveVars(d2.cons, d2.prims, d2.aux);
+
+    for (int var(0); var < d.Nprims; var++) {
+      for (int i(0); i < d.Nx; i++) {
+        for (int j(0); j < d.Ny; j++) {
+          EXPECT_NEAR(d.prims[d.id(var, i, j)], d2.prims[d.id(var, i, j)], tol);
+        }
+      }
+    }
+    for (int var(0); var < d.Naux; var++) {
+      for (int i(0); i < d.Nx; i++) {
+        for (int j(0); j < d.Ny; j++) {
+          EXPECT_NEAR(d.aux[d.id(var, i, j)], d2.aux[d.id(var, i, j)], tol);
+        }
+      }
+    }
+
+  }
+
+
 
 
 
@@ -82,86 +241,6 @@ namespace {
           else if (var == 2) EXPECT_EQ(d.aux[d.id(var, i, j)], 1.5);
           else if (var == 3) EXPECT_EQ(d.aux[d.id(var, i, j)], sqrt(10.0/21.0));
           else EXPECT_NEAR(d.aux[d.id(var, i, j)], 0.0, 1e-15);
-        }
-      }
-    }
-  }
-
-
-  /* ######################### Test source contribution ########################*/
-
-  TEST(SRMHD, SourceTerm)
-  {
-
-    // Set up
-    Data d(10, 10, 0, 1, 0, 1, 1.0, 0.5, 4, 5.0/3.0, 0.0, 0, 0, 0, 0, 0.5);
-    SRMHD model(&d);
-    Simulation sim(&d);
-
-    // Set cons to something
-    for (int i(0); i < d.Nx; i++) {
-      for (int j(0); j < d.Ny; j++) {
-        for (int var(0); var < d.Ncons; var++) {
-          d.cons[d.id(var, i, j)] = 3.1415926;
-        }
-      }
-    }
-    // Determine source
-    model.sourceTerm(d.cons, d.prims, d.aux, d.source);
-    for (int i(0); i < d.Nx; i++) {
-      for (int j(0); j < d.Ny; j++) {
-        for (int var(0); var < d.Ncons; var++) {
-          if (var == 8) EXPECT_EQ(d.source[d.id(var, i, j)], -3.1415926 / (0.25));
-          else EXPECT_EQ(d.source[d.id(var, i, j)], 0);
-        }
-      }
-    }
-  }
-
-
-  /* ######################### Test flux vector splitting ########################*/
-
-  TEST(SRMHD, FluxVectorSplittingStationary)
-  {
-
-    // Set up
-    Data d(10, 10, 0, 1, 0, 1, 1.0, 0.5, 4, 5.0/3.0, 0.0, 0, 0, 0, 0, 0.5);
-    SRMHD model(&d);
-    Simulation sim(&d);
-
-    // Set state to stationary equilibrium state
-    for (int i(0); i < d.Nx; i++) {
-      for (int j(0); j < d.Ny; j++) {
-        d.prims[d.id(0, i, j)] = 0.5; // Require non-zero density
-        d.prims[d.id(1, i, j)] = 0.0;
-        d.prims[d.id(2, i, j)] = 0.0;
-        d.prims[d.id(3, i, j)] = 0.0;
-        d.prims[d.id(4, i, j)] = 0.0;
-        d.prims[d.id(5, i, j)] = 0.0;
-        d.prims[d.id(6, i, j)] = 0.0;
-        d.prims[d.id(7, i, j)] = 0.0;
-        d.prims[d.id(8, i, j)] = 0.0;
-      }
-    }
-
-    model.primsToAll(d.cons, d.prims, d.aux);
-
-    // System is stationary, there should be zero flux
-    // x-direction
-    model.fluxFunc(d.cons, d.prims, d.aux, d.f, d.fnet, 0);
-    for (int i(0); i < d.Nx; i++) {
-      for (int j(0); j < d.Ny; j++) {
-        for (int var(0); var < d.Ncons; var++) {
-          EXPECT_EQ(d.fnet[d.id(var, i, j)], 0.0);
-        }
-      }
-    }
-    // y-direction
-    model.fluxFunc(d.cons, d.prims, d.aux, d.f, d.fnet, 1);
-    for (int i(0); i < d.Nx; i++) {
-      for (int j(0); j < d.Ny; j++) {
-        for (int var(0); var < d.Ncons; var++) {
-          EXPECT_EQ(d.fnet[d.id(var, i, j)], 0.0);
         }
       }
     }
