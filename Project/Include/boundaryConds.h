c0a4bf4ed4d8361f9e02b12304e5dce96d1d1285
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-12 16:45:21 +0100

    Outflow and Periodic boundaries added, not tested. Bcs derive from base class and define their own member apply()

--- Project/Include/boundaryConds.h
+++ Project/Include/boundaryConds.h
@@ -1,16 +1,60 @@
 #ifndef BOUNDARYCONDS_H
 #define BOUNDARYCONDS_H
 
-#include "simulation.h"
+#include "simData.h"
 
-  class Bcs
-  {
+//! Boundary Conditions
+/*!
+    Class requires simulation data with regards to the size of the domain. The
+  state vectors inside data are not necessarily the vectors that require the
+  bc functions. For those, the specific type of bc takes a pointer to the
+  vector to be formatted.
+*/
+class Bcs
+{
+  private:
+    Data * data;
 
-    public:
-      Bcs(*Simulation sim);
-      void outflow(**double);
-      void periodic(**double);
+  public:
+    //! Constructor store data about simulation (needed for domain)
+    Bcs(Data * simData) : data(data) { }
 
-  };
+    //! Application function
+    /*!
+        Each derived class must have a function to apply the corresponding
+      condition at the boundary.
+    */
+    virtual void apply(double *) = 0;
+};
+
+
+//! Outflow boundary conditions
+/*!
+    Imposes flows that exit and enter the domain, analogous to a domain that
+  extends to infinity in each direction.
+*/
+class Outflow : private Bcs
+{
+  private:
+    Data * data;
+
+  public:
+    void apply(double * cons);
+};
+
+
+//! Periodic boundary conditions
+/*!
+    Flows that exit across one domain boundary re-enter at the opposing
+  end.
+*/
+class Periodic : private Bcs
+{
+  private:
+    Data * data;
+    
+  public:
+    void apply(double * cons);
+};
 
 #endif
---
+++ Project/Src/boundaryConds.cu
@@ -0,0 +1,57 @@
+#include "boundaryConds.h"
+
+void Outflow::apply(double * cons)
+{
+  // Syntax
+  Data * d(this->data);
+
+  for (int var(0); var < d->Ncons; var++) {
+    for (int i(0); i < d->Ng; i++) {
+      for (int j(d->Ng); j < d->ny + d->Ng; j++) {
+        // Left
+        cons[d->id(var, i, j)] = cons[d->id(var, d->Ng, j)];
+        // Right
+        cons[d->id(var, d->nx + d->Ng + i, j)] = cons[d->id(var, d->nx + d->Ng - 1, j)];
+      }
+    }
+  }
+
+  for (int var(0); var < d->Ncons; var++) {
+    for (int i(d->Ng); i < d->nx + d->Ng; i++) {
+      for (int j(0); j < d->Ng; j++) {
+        // Bottom
+        cons[d->id(var, i, j)] = cons[d->id(var, i, d->Ng)];
+        // Top
+        cons[d->id(var, i, d->ny + d->Ng + j)] = cons[d->id(var, i, d->ny + d->Ng - 1)];
+      }
+    }
+  }
+}
+
+void Periodic::apply(double * cons)
+{
+  // Syntax
+  Data * d(this->data);
+  
+  for (int var(0); var < d->Ncons; var++) {
+    for (int i(0); i < d->Ng; i++) {
+      for (int j(d->Ng); j < d->ny + d->Ng; j++) {
+        // Left
+        cons[d->id(var, i, j)] = cons[d->id(var, d->nx + i, j)];
+        // Right
+        cons[d->id(var, d->nx + d->Ng + i, j)] = cons[d->id(var, d->Ng + i, j)];
+      }
+    }
+  }
+
+  for (int var(0); var < d->Ncons; var++) {
+    for (int i(d->Ng); i < d->nx + d->Ng; i++) {
+      for (int j(0); j < d->Ng; j++) {
+        // Bottom
+        cons[d->id(var, i, j)] = cons[d->id(var, i, d->nx + j)];
+        // Top
+        cons[d->id(var, i, d->ny + d->Ng + j)] = cons[d->id(var, i, d->Ng + j)];
+      }
+    }
+  }
+}
