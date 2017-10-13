b7d8ebbc69c2026f0196894191f5591e9c0a7337
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-13 16:56:39 +0100

    documentation

--- Project/Include/timeInt.h
+++ Project/Include/timeInt.h
@@ -5,7 +5,12 @@
 #include "model.h"
 #include "boundaryConds.h"
 
-
+//! General form of the time integrator
+/*!
+    Abstract base class for all types of time integrators. Integrators require
+  data to operate on, model for the flux functions and the boundary conditions
+  to apply one the step has been made
+*/
 class TimeIntegrator
 {
   protected:
@@ -14,8 +19,14 @@ class TimeIntegrator
     Bcs * bc;
 
   public:
-    TimeIntegrator() : data(NULL), model(NULL) { }
+    //! Constructor reuires simData, model type and boundary conditions
     TimeIntegrator(Data * data, Model * model, Bcs * bc) : data(data), model(model), bc(bc) { }
+
+    //! Step function
+    /*!
+        Pure virtual function: every time integrator must be able to increment
+      time forward by a single timestep, dt, given by the simData.
+    */
     virtual void step() = 0;
 
 };
