c94e602871ce8e39de49b7bbdf7579747da4b20a
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-13 16:56:18 +0100

    set() member stores the specific objects being used throughout

--- Project/Include/simulation.h
+++ Project/Include/simulation.h
@@ -4,6 +4,9 @@
 #include "initFunc.h"
 #include "simData.h"
 #include "model.h"
+#include "timeInt.h"
+#include "boundaryConds.h"
+
 
 //! The Simulation interface for the programme
 /*!
@@ -19,16 +22,20 @@ class Simulation
   private:
 
     //! Initial function object to set up starting data
-    InitialFunc * initFuncObj;
+    InitialFunc * init;
 
-    //! Form of simulation, governing equations and spectral analysis
-    Model * modelObj;
+    //! Form of simulation, governing equations and spectral decomposition
+    Model * model;
+
+    //! Time integrator object
+    TimeIntegrator * timeInt;
+
+    //! Boundary condition
+    Bcs * bcs;
 
     //! Incrememt the system forward by a single timestep
     void updateTime();
 
-    //! Implements the initial set up of prim vars
-    void initialize();
 
   public:
 
@@ -41,8 +48,11 @@ class Simulation
     //! Destructor frees alloc'd memory
     ~Simulation();
 
+    //! Stores the model type and general simulation form and sets the prim and aux vars
+    void set(InitialFunc * init, Model * model,
+             TimeIntegrator * timeInt, Bcs * bcs);
 
-    //!
+    //! Run the current set up until the end time
     void evolve();
 
 
