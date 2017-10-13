3a32e5fca8a46cf249a01de4ccdb53d7f5d2f3ca
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-13 16:11:22 +0100

    Tests more simulation data parameters

--- Tests/Src/test_simulation.cu
+++ Tests/Src/test_simulation.cu
@@ -22,6 +22,7 @@ namespace
     // Check standard data
     EXPECT_EQ(sim.data->nx, 100);
     EXPECT_EQ(sim.data->ny, 10);
+    EXPECT_EQ(sim.data->Ng, 4);
     EXPECT_EQ(sim.data->Nx, 108);
     EXPECT_EQ(sim.data->Ny, 18);
     EXPECT_EQ(sim.data->xmin, 0.0);
@@ -30,9 +31,19 @@ namespace
     EXPECT_EQ(sim.data->ymax, 0.5);
     EXPECT_EQ(sim.data->endTime, 0.8);
     EXPECT_EQ(sim.data->cfl, 0.5);
-    EXPECT_EQ(sim.data->Ng, 4);
     EXPECT_EQ(sim.data->gamma, 5.0/3);
     EXPECT_EQ(sim.data->sigma, 0.0);
+    EXPECT_EQ(sim.data->memSet, 1);
+    EXPECT_EQ(sim.data->Ncons, 9);
+    EXPECT_EQ(sim.data->Nprims, 8);
+    EXPECT_EQ(sim.data->Naux, 13);
+    EXPECT_EQ(sim.data->alphaX, 1.0);
+    EXPECT_EQ(sim.data->alphaY, 1.0);
+    EXPECT_EQ(sim.data->t, 0.0);
+    EXPECT_EQ(sim.data->dt, 0.005);
+    EXPECT_EQ(sim.data->dx, 0.01);
+    EXPECT_EQ(sim.data->dy, 0.1);
+    EXPECT_EQ(sim.data->iters, 0);
 
     // Check domain
     /* Note: Ng = 4 */
