dcb84488aa981a46d9a28ab1fffa15b17f10f401
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-17 12:53:36 +0100

    Playing

--- Project/Src/main.cu
+++ Project/Src/main.cu
@@ -10,7 +10,7 @@
 int main(void) {
 
   // Set up domain
-  Data data(100, 100, 0.0, 1.0, 0.0, 1.0, 0.01);
+  Data data(100, 100, 0.0, 1.0, 0.0, 1.0, 0.8);
 
   // Choose particulars of simulation
   SRMHD model(&data);
@@ -25,12 +25,7 @@ int main(void) {
 
   sim.set(&init, &model, &timeInt, &bcs);
 
-  printf("%18.16f\n", data.cons[data.id(1, 3, 3)]);
-
   sim.evolve();
-
-  printf("%18.16f\n", data.cons[data.id(1, 3, 3)]);
-
 
   return 0;
 
