9a79364090999cf81a18463d80f71dad1dfddf48
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-17 16:06:20 +0100

    Times execution

--- Project/Src/main.cu
+++ Project/Src/main.cu
@@ -4,13 +4,17 @@
 #include "srmhd.h"
 #include "boundaryConds.h"
 #include "rkSplit.h"
-#include <stdio.h>
+#include <cstdio>
+#include <ctime>
 
 
 int main(void) {
 
+  // Time execution of programme
+  int start_s=clock();
+
   // Set up domain
-  Data data(100, 100, 0.0, 1.0, 0.0, 1.0, 0.8);
+  Data data(30, 30, 0.0, 1.0, 0.0, 1.0, 0.1, 0.1);
 
   // Choose particulars of simulation
   SRMHD model(&data);
@@ -26,6 +30,37 @@ int main(void) {
   sim.set(&init, &model, &timeInt, &bcs);
 
   sim.evolve();
+
+  // for (int i(0); i < data.Nprims; i++) {
+  //   printf("prims[%d] = %18.16f\n", i, data.prims[data.id(i, 3, 3)]);
+  // } printf("\n");
+  //
+  // for (int i(0); i < data.Naux; i++) {
+  //   printf("aux[%d] = %18.16f\n", i, data.aux[data.id(i, 3, 3)]);
+  // } printf("\n");
+  //
+  // for (int i(0); i < data.Ncons; i++) {
+  //   printf("cons[%d] = %18.16f\n", i, data.cons[data.id(i, 3, 3)]);
+  // } printf("\n");
+  //
+  // sim.updateTime();
+  // sim.updateTime();
+  //
+  // for (int i(0); i < data.Nprims; i++) {
+  //   printf("prims[%d] = %18.16f\n", i, data.prims[data.id(i, 3, 3)]);
+  // } printf("\n");
+  //
+  // for (int i(0); i < data.Naux; i++) {
+  //   printf("aux[%d] = %18.16f\n", i, data.aux[data.id(i, 3, 3)]);
+  // } printf("\n");
+  //
+  // for (int i(0); i < data.Ncons; i++) {
+  //   printf("cons[%d] = %18.16f\n", i, data.cons[data.id(i, 3, 3)]);
+  // } printf("\n");
+
+
+  int stop_s=clock();
+  printf("\nRuntime: %.3fs\nCompleted %d iterations.\n", (stop_s-start_s)/double(CLOCKS_PER_SEC), data.iters);
 
   return 0;
 
