02dff437a9c05ce66ca65840b2f8d3c51baed752
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-18 17:00:25 +0100

    Documentation

--- Project/Include/saveData.h
+++ Project/Include/saveData.h
@@ -3,27 +3,47 @@
 
 #include "simData.h"
 
-//! Saves the current data
+//! Class used to save simulation data
 /*!
-
+    Class is initialized with the data that is to be saved. Saves the simulation
+  data in the Data directory, located within the Project folder. All data is
+  saved automatically, including all constant data (xmin, ymax, endTime etc) and
+  and the values of all prims, aux and cons variables.
 */
 class SaveData
 {
   private:
 
+    //! Saves all cons, prims, aux and constant data
+    /*!
+        This calls the other member functions to save their respective
+      simulation data. Automatically called by the constructor.
+    */
     void saveAll();
 
+    //! Saves the conserved vector state
     void saveCons();
 
+    //! Saves the primitive vector state
     void savePrims();
 
+    //! Saves the auxilliary vector state
     void saveAux();
 
+    // Saves the constant data
     void saveConsts();
 
+
   public:
+    //! Simulation data to be saved
     Data * d;
 
+    //! Constructor
+    /*!
+        The constructor take a pointer to the data class which the user wants
+      to save. All this data is automatically saved in the Data directory, located
+      in the Project folder.
+    */
     SaveData(Data * data) : d(data)
     {
       this->saveAll();
