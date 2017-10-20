5555568bc42fade2f3be39ae54a0782be445a3dc
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-20 22:24:54 +0100

    documentation

--- Project/Include/srmhd.h
+++ Project/Include/srmhd.h
@@ -5,9 +5,9 @@
 
 //! Special Relativistic MagnetHydroDynamics
 /*!
-    The special relativistic, ideal fluid limit of the general relativsitic
-  MHD equations. Ideal fluid, so resistivity does not play a part, hence no
-  electric fields due to freezing.
+    The single fluid, special relativistic, ideal limit of the MHD equations.
+  Ideal fluid, so resistivity does not play a part, hence no
+  electric field evolution.
   Model has nine conserved variables:
     D, Sx, Sy, Sz, tau, Bx, By, Bz, phi
   Eight primitive variables:
@@ -29,18 +29,52 @@ class SRMHD : public Model
     ~SRMHD() {}
 
     //! Source term contribution
+    /*!
+        Non-zero flux for cons[8], phi, as a result of implementing divergence
+      cleaning. For details see Muddle.
+    */
     void sourceTerm(double *cons, double *prims, double *aux, double *source);
 
-    //! Spectral analysis
+    //! Spectral decomposition
+    /*!
+        Method outlined in Anton 2010, `Relativistic Magnetohydrodynamcis:
+      Renormalized Eignevectors and Full Wave Decompostiion Riemann Solver`. Requires
+      an N=2 rootfind using cminpack library.
+      Initial inputs will be the current values of the conserved vector and the
+      OLD values for the prims and aux vectors.
+      Output will be the current values of cons, prims and aux.
+    */
     void getPrimitiveVars(double *cons, double *prims, double *aux);
 
     //! Primitive-to-all transformation
+    /*!
+        Transforms the initial state given in primitive form in to the conserved
+      vector state. Relations have been taken from Anton 2010, `Relativistic
+      Magnetohydrodynamcis: Renormalized Eignevectors and Full Wave Decompostion
+      Riemann Solver`
+    */
     void primsToAll(double *cons, double *prims, double *aux);
 
     //! Numerical flux function
+    /*!
+        Method determines the value of the conserved vector flux through the
+      cell faces.
+        We are using the flux vector splitting method described in Shu, `Essentially
+      Non-Oscillatory and Weighted Essentially Non-Oscillatory Schemes for Hyperbolic
+      Conservation Laws`. For the form of the fluxes see Relativistic Magneto..., Anton '10
+      with the inclusion of divergence cleaning from Advanced numerical methods for Neutron star
+      interfaces, John Muddle.
+        Note: We are assuming that all primitive and auxilliary variables are up-to-date
+      at the time of this function execution.
+    */
     void fluxFunc(double *cons, double *prims, double *aux, double *f, double *fnet, const int dir);
 
     //! Numerical flux approximation (solution stored in fnet)
+    /*!
+        Given the current values for the cons prims and aux vars, uses the flux
+      reconstruction method to determine the flux at the cell faces and computes 
+      the net flux of the conserved vector through each cell
+    */
     void F(double *cons, double *prims, double *aux, double *f, double *fnet);
 };
 
