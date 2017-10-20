ccbfb4f5711160568f89dea05cced52e36f7d5a1
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-10-20 15:34:30 +0100

    Tests that an equilibrium system will not change

--- Tests/Src/test_simulation.cu
+++ Tests/Src/test_simulation.cu
@@ -1,8 +1,10 @@
 #include "gtest/gtest.h"
-#include "simulation.h"
 #include "simData.h"
-#include "cudaErrorCheck.h"
+#include "simulation.h"
+#include "initFunc.h"
 #include "srmhd.h"
+#include "boundaryConds.h"
+#include "rkSplit.h"
 #include <cstdlib>
 #include <stdexcept>
 
@@ -59,6 +61,52 @@ namespace
     EXPECT_EQ(sim.data->y[1], -0.75);
     EXPECT_EQ(sim.data->x[99+4], 0.995);
 
+
+  }
+
+  //! Check that the fields dont change if the system if homogenous
+  TEST(Simulation, equilibriumSimulation)
+  {
+    Data data(30, 30, 10, 0, 1, 0, 1, 0, 1, 0.1);
+    SRMHD model(&data);
+    Simulation sim(&data);
+    OTVortex init(&data);
+    Periodic bcs(&data);
+    RKSplit timeInt(&data, &model, &bcs);
+
+    for (int i(0); i < data.Nx; i++) {
+      for (int j(0); j < data.Ny; j++) {
+        for (int k(0); k < data.Nz; k++) {
+          data.prims[data.id(0, i, j, k)] = 0.125;
+          data.prims[data.id(1, i, j, k)] = 0.1;
+          data.prims[data.id(2, i, j, k)] = 0.1;
+          data.prims[data.id(3, i, j, k)] = 0.1;
+          data.prims[data.id(4, i, j, k)] = 0.1;
+          data.prims[data.id(5, i, j, k)] = 0.125;
+          data.prims[data.id(6, i, j, k)] = 0.125;
+          data.prims[data.id(7, i, j, k)] = 0.125;
+        }
+      }
+    }
+
+    sim.set(&init, &model, &timeInt, &bcs);
+    sim.evolve();
+
+
+    for (int i(0); i < data.Nx; i++) {
+      for (int j(0); j < data.Ny; j++) {
+        for (int k(0); k < data.Nz; k++) {
+          EXPECT_NEAR(data.prims[data.id(0, i, j, k)], 0.125, 1e-15);
+          EXPECT_NEAR(data.prims[data.id(1, i, j, k)], 0.1, 1e-15);
+          EXPECT_NEAR(data.prims[data.id(2, i, j, k)], 0.1, 1e-15);
+          EXPECT_NEAR(data.prims[data.id(3, i, j, k)], 0.1, 1e-15);
+          EXPECT_NEAR(data.prims[data.id(4, i, j, k)], 0.1, 1e-15);
+          EXPECT_NEAR(data.prims[data.id(5, i, j, k)], 0.125, 1e-15);
+          EXPECT_NEAR(data.prims[data.id(6, i, j, k)], 0.125, 1e-15);
+          EXPECT_NEAR(data.prims[data.id(7, i, j, k)], 0.125, 1e-15);
+        }
+      }
+    }
 
   }
 
