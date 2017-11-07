f36a5ffaf28751172c03b2698aac89ef10a8f084
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-11-07 17:27:24 +0000

    Can plot 1D slices through fluid

--- Project/Src/interactivePlot.py
+++ Project/Src/interactivePlot.py
@@ -8,6 +8,12 @@ import numpy as np
 import pylab as plt
 from matplotlib import cm
 
+# Change this to the relative path to the data you want to plot
+# File names must start with e.g. `primitive`, anything between this
+# and `.dat` should be stored in appendix
+DataDirectory = '../Data/'
+appendix = ''
+
 def gatherData():
     """
     Collects and stores all the data required for plotting the final state of
@@ -15,19 +21,19 @@ def gatherData():
     
     Returns
     -------
-        cons: array of float
+        cons : array of float
             (Ncons, Nx, Ny, Nz) Array containing the conserved vector
-        consLabels: array of string
+        consLabels : array of string
             (Ncons,) The labels of the conserved elements
-        prims: array of float
+        prims : array of float
             (Nprims, Nx, Ny, Nz) Array containing the primitive vector
-        primLabels: array of string
+        primLabels : array of string
             (Nprims,) The labels of the primitive elements
-        aux: array of float
+        aux : array of float
             (Naux, Nx, Ny, Nz) Array containing the auxilliary vector
-        auxLabels: array of string
+        auxLabels : array of string
             (Naux,) The labels of the auxilliary elements
-        c: dictionary
+        c : dictionary
             Dictionary containing all constant data saved in simData. Access
             elements by typing as an argument the constant you want as a string.
             E.g. to get zmax, enter  -->    c['zmax']
@@ -39,7 +45,7 @@ def gatherData():
     c = {}
     # Get constants first
     print("Fetching constants...")
-    with open('../Data/constants.dat', 'r') as f:
+    with open(DataDirectory + 'constants' + appendix + '.dat', 'r') as f:
         for i, line in enumerate(f):
             if not i==0:
                 line=line.split()
@@ -70,11 +76,14 @@ def gatherData():
                 c['dy'] = float(line[24])
                 c['dz'] = float(line[25])
 
+    print(c['Ncons'])
+    print(c['Nprims'])
+    print(c['Naux'])
 
     # Now get primitive variables and store the data in array...
     prims = np.zeros([c['Nprims'], c['Nx'], c['Ny'], c['Nz']])
     print("Fetching primitive variables...")
-    with open('../Data/primitive.dat', 'r') as f:
+    with open(DataDirectory + 'primitive' + appendix + '.dat', 'r') as f:
         for i, line in enumerate(f):
             # Get primitive var labels
             if i==0:
@@ -96,7 +105,7 @@ def gatherData():
     # Now gather conserved data
     cons = np.zeros([c['Ncons'], c['Nx'], c['Ny'], c['Nz']])
     print("Fetching conserved variables...")
-    with open('../Data/conserved.dat', 'r') as f:
+    with open(DataDirectory + 'conserved' + appendix + '.dat', 'r') as f:
         for i, line in enumerate(f):
             # Get cons var labels
             if i==0:
@@ -118,7 +127,7 @@ def gatherData():
     # And finally the aux vars
     aux = np.zeros([c['Naux'], c['Nx'], c['Ny'], c['Nz']])
     print("Fetching auxilliary variables...")
-    with open('../Data/auxilliary.dat', 'r') as f:
+    with open(DataDirectory + 'auxilliary' + appendix +'.dat', 'r') as f:
         for i, line in enumerate(f):
             # Get cons var labels
             if i==0:
@@ -213,7 +222,7 @@ def getYIndexFromLine(line, Nx, Ny):
         index:
             The y-index of the current line's data.
     """
-    return (line-1)%Nx
+    return (line-1)%Ny
 
 def plotHeatMaps(data, dataLabels, c, color=None, axis=2):
     """
@@ -261,7 +270,66 @@ def plotHeatMaps(data, dataLabels, c, color=None, axis=2):
         fig.colorbar(surf, shrink=0.5, aspect=5)
         plt.legend()
         plt.show()
-
+        
+        
+def plotSlice(data, dataLabels, c, axis=0):
+    """
+    Plots the variation of data in the `axis` direction. 
+    
+    Parameters
+    ----------
+        data: array of float
+            (Nvars, Nx, Ny, Nz) The data to be plotted (e.g. prims)
+        dataLabels: array of string
+            (Nvars,) The corresponding labels for the elements of data
+        c: dictionary
+            The simulation constant data
+        color: matplotlib color map
+            The colour theme to be plotting in. This can take string arguments
+            but best to stick to variants of cm.somecolourscheme
+            E.g. cm.magma
+        axis: int, optional
+            The axis the user wants to plot in.
+            (0, 1, 2) = (x, y, z)
+            Defaults to axis=0, x-direction.
+    """
+    Nx, Ny, Nz, Ng= c['Nx'], c['Ny'], c['Nz'], c['Ng']
+    
+    for i in range(len(data)):
+        plt.figure()
+        if (axis == 0):
+            plotVars = data[i, Ng:-Ng, Ny//2, Nz//2]
+            axisLabel = r'$x$'
+            step = c['dx']
+            n = c['nx']
+            left, right = c['xmin'], c['xmax']
+        if (axis == 1):
+            plotVars = data[i, Nx//2, Ng:-Ng, Nz//2]
+            axisLabel = r'$y$'
+            step = c['dy']
+            n = c['ny']
+            left, right = c['ymin'], c['ymax']
+        if (axis == 2):
+            plotVars = data[i, Nx//2, Ny//2, Ng:-Ng]
+            axisLabel = r'$z$'
+            step = c['dz']
+            n = c['nz']
+            left, right = c['zmin'], c['zmax']
+            
+        ymin = np.min(plotVars)
+        ymax = np.max(plotVars)
+        rangeY = ymax - ymin
+        ylower = ymin - 0.025 * rangeY
+        yupper = ymax + 0.025 * rangeY
+        xs = np.linspace(left + step/2, right - step/2, n)
+        plt.plot(xs, plotVars)
+        plt.title(r'Time Evolution for {}: $t = {}$'.format(dataLabels[i], c['t']))
+        plt.xlabel(axisLabel)
+        plt.ylabel(r'$q_{}(x)$'.format(i+1))
+        plt.ylim((ylower, yupper))
+        plt.legend(loc='lower center', fontsize=10)
+#            py.savefig('Figures/GR2MHD{}.pdf'.format(self.primLabels[i]))
+        plt.show()
 
 
 # Function declarations over, access data and plot!
