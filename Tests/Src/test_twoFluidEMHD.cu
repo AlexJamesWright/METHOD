4956d02b972a03d596f8ddac528ad7a9dfb360c0
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-11-14 17:26:20 +0000

    tests prims2cons2prims, ensures values are unchanged after function and its inverse

--- Tests/Src/test_twoFluidEMHD.cu
+++ Tests/Src/test_twoFluidEMHD.cu
@@ -38,9 +38,6 @@ namespace
       |  /
       | /
       |/_______> axis 1 (x)
-
-
-
     */
     TEST(TwoFluidEMHD, FluxFunctionIsConsistentUponRotation)
     {
@@ -65,7 +62,6 @@ namespace
       RKSplit timeInty(&dy, &modely, &bcsy);
       simy.set(&inity, &modely, &timeInty, &bcsy);
       printf("Stepping y-discontinuity...\n");
-
       simy.updateTime();
 
       // Discontinuity in z direction
@@ -79,59 +75,157 @@ namespace
       printf("Stepping z-discontinuity...\n");
       simz.updateTime();
 
-      /*
-      for (int var(0); var < dx.Ncons; var++) {
-        for (int i(0); i < dx.Nx; i++) {
-          for (int j(0); j < dy.Ny; j++) {
-            for (int k(0); k < dz.Nz; k++) {
-              EXPECT_NEAR(dx.cons[dx.id(var, i, j, k)], dy.cons[dy.id(var, j, i, k)], 1e-15);
-              EXPECT_NEAR(dx.cons[dx.id(var, i, j, k)], dz.cons[dy.id(var, k, j, i)], 1e-15);
-              EXPECT_NEAR(dy.cons[dx.id(var, i, j, k)], dz.cons[dy.id(var, i, k, j)], 1e-15);
+
+      for (int i(0); i < dx.Nx; i++) {
+        for (int j(0); j < dy.Ny; j++) {
+          for (int k(0); k < dz.Nz; k++) {
+
+            // Swap x and y
+            EXPECT_NEAR(dx.cons[dx.id(0, i, j, k)], dy.cons[dy.id(0, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(1, i, j, k)], dy.cons[dy.id(2, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(2, i, j, k)], dy.cons[dy.id(1, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(3, i, j, k)], dy.cons[dy.id(3, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(4, i, j, k)], dy.cons[dy.id(4, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(5, i, j, k)], dy.cons[dy.id(5, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(6, i, j, k)], dy.cons[dy.id(7, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(7, i, j, k)], (-1 * dy.cons[dy.id(6, j, i, k)]), 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(8, i, j, k)], (-1 * dy.cons[dy.id(8, j, i, k)]), 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(9, i, j, k)], dy.cons[dy.id(9, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(10, i, j, k)], dy.cons[dy.id(11, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(11, i, j, k)], dy.cons[dy.id(10, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(12, i, j, k)], dy.cons[dy.id(12, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(13, i, j, k)], dy.cons[dy.id(14, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(14, i, j, k)], (-1 * dy.cons[dy.id(13, j, i, k)]), 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(15, i, j, k)], (-1 * dy.cons[dy.id(15, j, i, k)]), 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(16, i, j, k)], dy.cons[dy.id(16, j, i, k)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(17, i, j, k)], dy.cons[dy.id(17, j, i, k)], 1e-15);
+
+
+            // Swap x and z
+            EXPECT_NEAR(dx.cons[dx.id(0, i, j, k)], dz.cons[dz.id(0, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(1, i, j, k)], dz.cons[dz.id(3, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(2, i, j, k)], dz.cons[dz.id(2, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(3, i, j, k)], dz.cons[dz.id(1, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(4, i, j, k)], dz.cons[dz.id(4, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(5, i, j, k)], dz.cons[dz.id(5, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(6, i, j, k)], dz.cons[dz.id(8, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(7, i, j, k)], (-1 * dz.cons[dz.id(7, k, j, i)]), 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(8, i, j, k)], (-1 * dz.cons[dz.id(6, k, j, i)]), 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(9, i, j, k)], dz.cons[dz.id(9, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(10, i, j, k)], dz.cons[dz.id(12, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(11, i, j, k)], dz.cons[dz.id(11, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(12, i, j, k)], dz.cons[dz.id(10, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(13, i, j, k)], dz.cons[dz.id(15, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(14, i, j, k)], (-1 * dz.cons[dz.id(14, k, j, i)]), 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(15, i, j, k)], (-1 * dz.cons[dz.id(13, k, j, i)]), 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(16, i, j, k)], dz.cons[dz.id(16, k, j, i)], 1e-15);
+            EXPECT_NEAR(dx.cons[dx.id(17, i, j, k)], dz.cons[dz.id(17, k, j, i)], 1e-15);
+
+
+            // Swap y and z
+            EXPECT_NEAR(dy.cons[dy.id(0, i, j, k)], dz.cons[dz.id(0, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(1, i, j, k)], dz.cons[dz.id(1, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(2, i, j, k)], dz.cons[dz.id(3, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(3, i, j, k)], dz.cons[dz.id(2, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(4, i, j, k)], dz.cons[dz.id(4, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(5, i, j, k)], dz.cons[dz.id(5, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(6, i, j, k)], (-1 * dz.cons[dz.id(6, i, k, j)]), 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(7, i, j, k)], dz.cons[dz.id(8, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(8, i, j, k)], (-1 * dz.cons[dz.id(7, i, k, j)]), 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(9, i, j, k)], dz.cons[dz.id(9, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(10, i, j, k)], dz.cons[dz.id(10, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(11, i, j, k)], dz.cons[dz.id(12, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(12, i, j, k)], dz.cons[dz.id(11, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(13, i, j, k)], (-1 * dz.cons[dz.id(13, i, k, j)]), 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(14, i, j, k)], dz.cons[dz.id(15, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(15, i, j, k)], (-1 * dz.cons[dz.id(14, i, k, j)]), 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(16, i, j, k)], dz.cons[dz.id(16, i, k, j)], 1e-15);
+            EXPECT_NEAR(dy.cons[dy.id(17, i, j, k)], dz.cons[dz.id(17, i, k, j)], 1e-15);
+
+          }
+        }
+      }
+    }
+
+
+    TEST(TwoFluidEMHD, Prims2Cons2Prims)
+    {
+      const double tol = 1.49011612e-8;   // Tolerance of rootfinder
+      Data d(10, 10, 10, 0, 1, 0, 1, 0, 1, 0.8);
+      TwoFluidEMHD model(&d);
+      Simulation sim(&d);
+      BrioWuTwoFluid init(&d);
+
+      Data d2(10, 10, 10, 0, 1, 0, 1, 0, 1, 0.8);
+      TwoFluidEMHD model2(&d2);
+      Simulation sim2(&d2);
+      BrioWuTwoFluid init2(&d2);
+
+      model2.primsToAll(d2.cons, d2.prims, d2.aux);
+      model.primsToAll(d.cons, d.prims, d.aux);
+
+
+      for (int var(0); var < d.Nprims; var++) {
+        for (int i(0); i < d.Nx; i++) {
+          for (int j(0); j < d.Ny; j++) {
+            for (int k(0); k < d.Nz; k++) {
+              EXPECT_NEAR(d.prims[d.id(var, i, j, k)], d2.prims[d.id(var, i, j, k)], tol);
             }
           }
         }
       }
-      */
+      for (int var(0); var < d.Naux; var++) {
+        for (int i(0); i < d.Nx; i++) {
+          for (int j(0); j < d.Ny; j++) {
+            for (int k(0); k < d.Nz; k++) {
+              EXPECT_NEAR(d.aux[d.id(var, i, j, k)], d2.aux[d.id(var, i, j, k)], tol);
+            }
+          }
+        }
+      }
 
-      // FAILS: var = [1, 2, 3, 4, 6, 7]
-      // *** Its spitting out nan all over the place. Im going home.
-      int var(8);
-      int i(9);
-      int j(8);
-      int k(8);
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dy.cons[dy.id(var, j, i, k)]), 1e-15);
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, k, j, i)]), 1e-15);
-      EXPECT_NEAR(fabs(dy.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, i, k, j)]), 1e-15);
-      i = 8; j=9; k=8;
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dy.cons[dy.id(var, j, i, k)]), 1e-15);
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, k, j, i)]), 1e-15);
-      EXPECT_NEAR(fabs(dy.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, i, k, j)]), 1e-15);
-      i = 8; j=8; k=9;
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dy.cons[dy.id(var, j, i, k)]), 1e-15);
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, k, j, i)]), 1e-15);
-      EXPECT_NEAR(fabs(dy.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, i, k, j)]), 1e-15);
-      i = 9; j=9; k=8;
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dy.cons[dy.id(var, j, i, k)]), 1e-15);
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, k, j, i)]), 1e-15);
-      EXPECT_NEAR(fabs(dy.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, i, k, j)]), 1e-15);
-      i = 9; j=8; k=9;
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dy.cons[dy.id(var, j, i, k)]), 1e-15);
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, k, j, i)]), 1e-15);
-      EXPECT_NEAR(fabs(dy.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, i, k, j)]), 1e-15);
-      i = 8; j=9; k=9;
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dy.cons[dy.id(var, j, i, k)]), 1e-15);
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, k, j, i)]), 1e-15);
-      EXPECT_NEAR(fabs(dy.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, i, k, j)]), 1e-15);
-      i = 9; j=9; k=9;
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dy.cons[dy.id(var, j, i, k)]), 1e-15);
-      EXPECT_NEAR(fabs(dx.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, k, j, i)]), 1e-15);
-      EXPECT_NEAR(fabs(dy.cons[dx.id(var, i, j, k)]), fabs(dz.cons[dy.id(var, i, k, j)]), 1e-15);
+      // Set all d2 prims slightly off so rootfind has to do something
+      for (int var(0); var < d.Nprims; var++) {
+        for (int i(0); i < d.Nx; i++) {
+          for (int j(0); j < d.Ny; j++) {
+            for (int k(0); k < d.Nz; k++) {
+              d2.prims[d.id(var, i, j, k)] *= 0.9;
+            }
+          }
+        }
+      }
+      for (int var(0); var < d.Naux; var++) {
+        for (int i(0); i < d.Nx; i++) {
+          for (int j(0); j < d.Ny; j++) {
+            for (int k(0); k < d.Nz; k++) {
+              d2.aux[d.id(var, i, j, k)] *= 0.9;
+            }
+          }
+        }
+      }
+      
+      // Solve and re-check
+      model2.getPrimitiveVars(d2.cons, d2.prims, d2.aux);
 
-
+      for (int var(0); var < d.Nprims; var++) {
+        for (int i(0); i < d.Nx; i++) {
+          for (int j(0); j < d.Ny; j++) {
+            for (int k(0); k < d.Nz; k++) {
+              EXPECT_NEAR(d.prims[d.id(var, i, j, k)], d2.prims[d.id(var, i, j, k)], tol);
+            }
+          }
+        }
+      }
+      for (int var(0); var < d.Naux; var++) {
+        for (int i(0); i < d.Nx; i++) {
+          for (int j(0); j < d.Ny; j++) {
+            for (int k(0); k < d.Nz; k++) {
+              EXPECT_NEAR(d.aux[d.id(var, i, j, k)], d2.aux[d.id(var, i, j, k)], tol);
+            }
+          }
+        }
+      }
     }
-
-
-
 
 
 }
