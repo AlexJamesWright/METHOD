3d5aa67e6c5648a6f4955baf024856e29e999118
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-11-17 16:14:59 +0000

    WS

--- Project/Src/rkSplit.cu
+++ Project/Src/rkSplit.cu
@@ -14,21 +14,19 @@ void RKSplit::step(double * cons, double * prims, double * aux)
   this->model->sourceTerm(cons, prims, aux, d->source);
 
   for (int var(0); var < d->Ncons; var++) {
-   for (int i(0); i < d->Nx; i++) {
-     for (int j(0); j < d->Ny; j++) {
-       for (int k(0); k < d->Nz; k++) {
-         cons[d->id(var, i, j, k)] += d->dt * d->source[d->id(var, i, j, k)];
-       }
-     }
-   }
+    for (int i(0); i < d->Nx; i++) {
+      for (int j(0); j < d->Ny; j++) {
+        for (int k(0); k < d->Nz; k++) {
+          cons[d->id(var, i, j, k)] += d->dt * d->source[d->id(var, i, j, k)];
+        }
+      }
+    }
   }
-
 
   // Determine new prim and aux variables
   this->model->getPrimitiveVars(cons, prims, aux);
 
   // Apply boundary conditions
   this->bc->apply(cons, prims, aux);
-
 
 }
