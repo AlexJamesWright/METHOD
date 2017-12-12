a0f82ee00af26b0d5239e80a2c8b65fed61833d6
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-12-12 14:04:23 +0000

    Corrected initial data for current sheet to amanos

--- Project/Src/initFunc.cu
+++ Project/Src/initFunc.cu
@@ -45,18 +45,24 @@ CurrentSheetTwoFluid::CurrentSheetTwoFluid(Data * data) : InitialFunc(data)
   if (d->Nprims != 16) throw std::runtime_error("Trying to implement a two fluid initial state on incorrect model.\n\tModel has wrong number of primitive variables to be two fluid model.");
   if (d->xmin != -1.5 || d->xmax != 1.5) throw std::runtime_error("Domain has incorrect values. Expected x E [-1.5, 1.5]\n");
 
+  double B0(1);
+  const double PI(3.14159265358979323);
+  const double rho(1.0);
+  const double p(50.0);
+
   for (int i(0); i < d->Nx; i++) {
     for (int j(0); j < d->Ny; j++) {
       for (int k(0); k < d->Nz; k++) {
-        d->prims[d->id(0, i, j, k)] = 0.5;
-        d->prims[d->id(4, i, j, k)] = 25.0;
-        d->prims[d->id(5, i, j, k)] = 0.5;
-        d->prims[d->id(9, i, j, k)] = 25.0;
-        d->prims[d->id(11, i, j, k)] = erf(0.5 * d->x[i] * sqrt(d->sigma));
+        d->prims[d->id(0, i, j, k)] = rho / 2.0;
+        d->prims[d->id(3, i, j, k)] = (B0 / (d->mu1 * rho) * sqrt(d->sigma / PI)) * exp(-d->x[i] * d->x[i] * d->sigma / 4.0);
+        d->prims[d->id(4, i, j, k)] = p / 2.0;
+        d->prims[d->id(5, i, j, k)] = rho / 2.0;
+        d->prims[d->id(8, i, j, k)] = (B0 / (d->mu2 * rho) * sqrt(d->sigma / PI)) * exp(-d->x[i] * d->x[i] * d->sigma / 4.0);
+        d->prims[d->id(9, i, j, k)] = p / 2.0;
+        d->prims[d->id(11, i, j, k)] = B0 * erf(0.5 * d->x[i] * sqrt(d->sigma));
       }
     }
   }
-
 }
 
 
