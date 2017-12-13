ad8280f916d2e09eba8ef42368fd90c151b8f3f3
AlexJamesWright <a.j.wright@soton.ac.uk>
2017-12-13 16:59:43 +0000

    CPAlfven wave for single and two fluid

--- Project/Src/initFunc.cu
+++ Project/Src/initFunc.cu
@@ -37,13 +37,114 @@ InitialFunc::InitialFunc(Data * data) : data(data)
   }
 }
 
+CPAlfvenWaveTwoFluid::CPAlfvenWaveTwoFluid(Data * data) : InitialFunc(data)
+{
+  // Syntax
+  Data * d(data);
+
+  const double PI(3.14159265358979323);
+
+  // Check Boundaries
+  if (d->xmin != 0.0) throw std::invalid_argument("Domain has incorrect values. Expected xmin = 0.0.\n");
+  if (d->xmax + 1e-10 < 8*PI || d->xmax - 1e-10 > 8*PI) throw std::invalid_argument("Domain has incorrect values. Expected xmax = 8pi.\n");
+  // Ensure correct model
+  if (d->Nprims != 16) throw std::invalid_argument("Trying to implement a two fluid initial state on incorrect model.\n\tModel has wrong number of primitive variables to be two fluid model.");
+  if (d->ny != 0) throw std::invalid_argument("System must be one dimensional.\n");
+  if (fabs(d->mu1) != sqrt(26.0/25.0)|| fabs(d->mu2) != sqrt(26.0/25.0)) throw std::invalid_argument("Charge mass ratio too large. \nset to mu = sqrt(26/25) and try again.\n");
+
+  double B0(1.04);
+  double omegaBar1(-sqrt(1.04));
+  double omegaBar2(sqrt(1.04));
+  double kx(1.0/4.0);
+
+  // Case 3
+  double omega(5.63803828148e-1);
+  double Wp(5.19940020571e-6+1);
+  double We(6.68453076522e-5+1);
+  double Tom(1.0e-2);
+  double xsi(0.01);
+
+  double U1(-xsi * omega * omegaBar1 / (kx * (omega + omegaBar1 / We)));
+  double U2(-xsi * omega * omegaBar2 / (kx * (omega + omegaBar2 / Wp)));
+
+  for (int i(0); i < d->Nx; i++) {
+    for (int j(0); j < d->Ny; j++) {
+      for (int k(0); k < d->Nz; k++) {
+
+        double phi(kx * d->x[i]);
+
+        // Density and pressure
+
+        d->prims[d->id(0, i, j, k)] = 1.0 / We;
+        d->prims[d->id(4, i, j, k)] = Tom * d->prims[d->id(0, i, j, k)];
+        d->prims[d->id(5, i, j, k)] = 1.0 / Wp;
+        d->prims[d->id(9, i, j, k)] = Tom * d->prims[d->id(5, i, j, k)];
+
+        // Bx, By, Bz
+        d->prims[d->id(10, i, j, k)] = B0;
+        d->prims[d->id(11, i, j, k)] = xsi * B0 * cos(phi);
+        d->prims[d->id(12, i, j, k)] = - xsi * B0 * sin(phi);
+
+        // Ey, Ez
+        d->prims[d->id(14, i, j, k)] = - (omega / kx) * xsi * B0 * sin(phi);
+        d->prims[d->id(15, i, j, k)] = - (omega / kx) * xsi * B0 * cos(phi);
+
+        // vy1, vz1
+        d->prims[d->id(2, i, j, k)] = U1 * cos(phi) / We;
+        d->prims[d->id(3, i, j, k)] = -U1 * sin(phi) / We;
+
+        // vy2, vz2
+        d->prims[d->id(7, i, j, k)] = U2 * cos(phi) / Wp;
+        d->prims[d->id(8, i, j, k)] = -U2 * sin(phi) / Wp;
+
+      }
+    }
+  }
+
+}
+
+CPAlfvenWaveSingleFluid::CPAlfvenWaveSingleFluid(Data * data) : InitialFunc(data)
+{
+  // Syntax
+  Data * d(data);
+  // Check Boundaries
+  if (d->xmin != -0.5 || d->xmax != 0.5) throw std::invalid_argument("Domain has incorrect values. Expected x E [-0.5, 0.5]\n");
+  // Ensure correct model
+  if (d->Nprims > 15) throw std::invalid_argument("Trying to implement a single fluid initial state on multifluid model.\nModel has too many primitive variables to be single fluid.");
+  if (d->ny > 0) throw std::invalid_argument("System must be one dimensional for this initial set up. \nSet ny = nz = 0 and try again.\n");
+
+  double va(0.5);
+  double B0(1.1547);
+  const double PI(3.14159265358979323);
+
+
+  for (int i(0); i < d->Nx; i++) {
+    for (int j(0); j < d->Ny; j++) {
+      for (int k(0); k < d->Nz; k++) {
+        // Density and pressure
+        d->prims[d->id(0, i, j, k)] = 1.0;
+        d->prims[d->id(4, i, j, k)] = 1.0;
+
+        // By, vy
+        d->prims[d->id(6, i, j, k)] = B0 * cos(2 * PI * d->x[i]);
+        d->prims[d->id(2, i, j, k)] = -va / B0 * d->prims[d->id(6, i, j, k)];
+
+        // Bz, vz
+        d->prims[d->id(7, i, j, k)] = B0 * sin(2 * PI * d->x[i]);
+        d->prims[d->id(3, i, j, k)] = -va / B0 * d->prims[d->id(7, i, j, k)];
+      }
+    }
+  }
+
+}
+
 CurrentSheetTwoFluid::CurrentSheetTwoFluid(Data * data) : InitialFunc(data)
 {
   // Syntax
   Data * d(data);
 
-  if (d->Nprims != 16) throw std::runtime_error("Trying to implement a two fluid initial state on incorrect model.\n\tModel has wrong number of primitive variables to be two fluid model.");
-  if (d->xmin != -1.5 || d->xmax != 1.5) throw std::runtime_error("Domain has incorrect values. Expected x E [-1.5, 1.5]\n");
+  if (d->Nprims != 16) throw std::invalid_argument("Trying to implement a two fluid initial state on incorrect model.\n\tModel has wrong number of primitive variables to be two fluid model.");
+  if (d->xmin != -1.5 || d->xmax != 1.5) throw std::invalid_argument("Domain has incorrect values. Expected x E [-1.5, 1.5]\n");
 
   double B0(1);
   const double PI(3.14159265358979323);
@@ -82,8 +183,8 @@ OTVortexSingleFluid::OTVortexSingleFluid(Data * data) : InitialFunc(data)
     std::cout << "Boundaries are not as expected for OTVortex, uasually (x,y) E [0, 1]" << std::endl;
   }
   // Ensure correct model
-  if (d->Nprims > 15) throw std::runtime_error("Trying to implement a single fluid initial state on multifluid model.\nModel has too many primitive variables to be single fluid.");
-  if (d->ny == 0) throw std::runtime_error("System must be at least two dimensional for this initial set up. \nSet ny > 0 and try again.\n");
+  if (d->Nprims > 15) throw std::invalid_argument("Trying to implement a single fluid initial state on multifluid model.\nModel has too many primitive variables to be single fluid.");
+  if (d->ny == 0) throw std::invalid_argument("System must be at least two dimensional for this initial set up. \nSet ny > 0 and try again.\n");
 
   for (int i(0); i < d->Nx; i++) {
     for (int j(0); j < d->Ny; j++) {
@@ -110,7 +211,7 @@ BrioWuTwoFluid::BrioWuTwoFluid(Data * data, int dir, int setUp) : InitialFunc(da
   // Syntax
   Data * d(data);
   // Ensure correct model
-  if (d->Nprims != 16) throw std::runtime_error("Trying to implement a two fluid initial state on incorrect model.\n\tModel has wrong number of primitive variables to be two fluid model.");
+  if (d->Nprims != 16) throw std::invalid_argument("Trying to implement a two fluid initial state on incorrect model.\n\tModel has wrong number of primitive variables to be two fluid model.");
   // Ensure even number of cells to prevent zero in initial data at centre of domain
   if (d->nx%2 || d->ny%2 || d->nz%2) {
     throw std::invalid_argument("Please ensure even number of cells in each direction for Brio Wu initial data.\n");
