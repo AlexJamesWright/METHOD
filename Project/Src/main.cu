70a4eb92f0bdd01d51be40a03161dcb134640acf
AlexJamesWright <a.j.wright@soton.ac.uk>
2018-01-12 15:39:33 +0000

    Playing with srrmhd---SSP2 integrator not working fully

--- Project/Src/main.cu
+++ Project/Src/main.cu
@@ -2,6 +2,7 @@
 #include "simulation.h"
 #include "initFunc.h"
 #include "srmhd.h"
+#include "srrmhd.h"
 #include "twoFluidEMHD.h"
 #include "boundaryConds.h"
 #include "rkSplit.h"
@@ -21,20 +22,20 @@ int main(void) {
   const double PI(3.14159265358979323);
   const double MU(1000);
   // Set up domain
-  int nx(400);
+  int nx(256);
   int ny(0);
   int nz(0);
-  double xmin(0.0);
-  double xmax(1);
+  double xmin(-0.5);
+  double xmax(0.5);
   double ymin(0.0);
   double ymax(1.0);
   double zmin(0.0);
   double zmax(1.0);
-  double endTime(0.3);
-  double cfl(0.01);
+  double endTime(0.4);
+  double cfl(0.3);
   int Ng(4);
-  double gamma(2.0);
-  double sigma(1.0e17);
+  double gamma(5.0/3.0);
+  double sigma(0);
   double cp(1.0);
   double mu1(-MU);
   double mu2(MU);
@@ -44,23 +45,18 @@ int main(void) {
 
   // Choose particulars of simulation
   // SRMHD model(&data);
-  TwoFluidEMHD model(&data);
+  SRRMHD model(&data);
 
   FVS fluxMethod(&data, &model);
 
   Simulation sim(&data);
 
-  int dir(0); // x-direction
-  int setUp(1); // Amano set up
-  BrioWuTwoFluid init(&data, dir, setUp);
-  // CurrentSheetTwoFluid init(&data);
-  // CPAlfvenWaveTwoFluid init(&data);
+  // int dir(0); // x-direction
+  BrioWuSingleFluid init(&data);
 
   Outflow bcs(&data);
-  // Periodic bcs(&data);
 
-  RKSplit timeInt(&data, &model, &bcs, &fluxMethod);
-  // BackwardsRK2 timeInt(&data, &model, &bcs, &fluxMethod);
+  SSP2 timeInt(&data, &model, &bcs, &fluxMethod);
 
   // Now objects have been created, set up the simulation
   sim.set(&init, &model, &timeInt, &bcs, &fluxMethod);
@@ -69,14 +65,14 @@ int main(void) {
 
   // // Run until end time and save results
 
-  SaveData save(&data);
-  while (data.t < data.endTime) {
-    sim.updateTime();
-    save.saveAll();
-  }
-  // sim.evolve();
-  // sim.updateTime();
   // SaveData save(&data);
+  // while (data.t < data.endTime) {
+  //   sim.updateTime();
+  //   save.saveAll();
+  // }
+  sim.evolve();
+  // sim.updateTime();
+  SaveData save(&data);
 
   // SaveData save(&data);
   // sim.updateTime();
