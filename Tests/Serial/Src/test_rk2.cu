7940ad26b89ff904a1fc69107d0cda49d4755e58
AlexJamesWright <a.j.wright@soton.ac.uk>
2018-04-11 16:41:31 +0100

    Tests rk2 integrator against serial version

--- Tests/Serial/Src/test_rk2.cu
+++ Tests/Serial/Src/test_rk2.cu
@@ -1,13 +1,11 @@
 #include "gtest/gtest.h"
-#include "twoFluidEMHD.h"
+#include "srmhd.h"
 #include "simulation.h"
 #include "simData.h"
 #include "initFunc.h"
 #include "RK2.h"
 #include "fluxVectorSplitting.h"
 #include <cstdlib>
-#include <cmath>
-#include <cstdio>
 
 namespace
 {
@@ -20,17 +18,26 @@ namespace
          version with. No tests are run in the serial version of this test
       */
 
-      // Data d(30, 30, 30, 0, 1, 0, 1, 0, 1, 0.8);
-      // TwoFluidEMHD model(&d);
-      // FVS fluxMethod(&d, &model);
-      // Simulation sim(&d);
-      // BrioWuTwoFluid init(&d, 0, 0);
-      // Outflow bcs(&d);
-      // RK2 timeInt(&d, &model, &bcs, &fluxMethod);
-      // SaveData save(&d);
-      // sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
-      //
-      // sim.updateTime();
+      Data d(30, 30, 30, 0, 1, 0, 1, 0, 1, 0.05);
+      SRMHD model(&d);
+      FVS fluxMethod(&d, &model);
+      Simulation sim(&d);
+      OTVortexSingleFluid init(&d);
+      Outflow bcs(&d);
+      RK2 timeInt(&d, &model, &bcs, &fluxMethod);
+      SaveData save(&d);
+      sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
+
+      sim.evolve();
+
+
+      // Save data in test directory
+      strcpy(save.dir, "../TestData/Serial");
+      strcpy(save.app, "RK2");
+      save.saveCons();
+      save.savePrims();
+      save.saveAux();
+      save.saveConsts();
 
 
 
