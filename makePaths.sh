b615067413fce9b59a2fc16a1c6a005458124132
AlexJamesWright <a.j.wright@soton.ac.uk>
2018-08-22 17:04:25 +0100

    running 'bash makePaths.sh' from project root will generate the required subdirectories for saving data

---
+++ makePaths.sh
@@ -0,0 +1,37 @@
+mkdir Project/Parallel/Data
+mkdir Project/Parallel/Data/Final
+mkdir Project/Parallel/Data/Final/Auxilliary
+mkdir Project/Parallel/Data/Final/Conserved
+mkdir Project/Parallel/Data/Final/Primitive
+mkdir Project/Parallel/Data/Final/Constants
+mkdir Project/Parallel/Data/Final/Domain
+mkdir Project/Parallel/Data/TimeSeries
+mkdir Project/Parallel/Data/TimeSeries/Auxilliary
+mkdir Project/Parallel/Data/TimeSeries/Conserved
+mkdir Project/Parallel/Data/TimeSeries/Constants
+mkdir Project/Parallel/Data/TimeSeries/Primitive
+mkdir Project/Parallel/Data/TimeSeries/UserDef
+
+mkdir Project/Serial/Data
+mkdir Project/Serial/Data/Final
+mkdir Project/Serial/Data/Final/Auxilliary
+mkdir Project/Serial/Data/Final/Conserved
+mkdir Project/Serial/Data/Final/Primitive
+mkdir Project/Serial/Data/Final/Constants
+mkdir Project/Serial/Data/Final/Domain
+mkdir Project/Serial/Data/TimeSeries
+mkdir Project/Serial/Data/TimeSeries/Auxilliary
+mkdir Project/Serial/Data/TimeSeries/Conserved
+mkdir Project/Serial/Data/TimeSeries/Constants
+mkdir Project/Serial/Data/TimeSeries/Primitive
+mkdir Project/Serial/Data/TimeSeries/UserDef
+
+mkdir Tests/TestData/Parallel/Auxilliary
+mkdir Tests/TestData/Parallel/Conserved
+mkdir Tests/TestData/Parallel/Constants
+mkdir Tests/TestData/Parallel/Primitive
+
+mkdir Tests/TestData/Serial/Auxilliary
+mkdir Tests/TestData/Serial/Conserved
+mkdir Tests/TestData/Serial/Constants
+mkdir Tests/TestData/Serial/Primitive
