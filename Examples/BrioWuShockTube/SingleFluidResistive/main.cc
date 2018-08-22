b56cefa9e77224363d8982138efaca0ec3bbfba1
AlexJamesWright <a.j.wright@soton.ac.uk>
2018-08-22 15:54:37 +0100

    Few examples

---
+++ Examples/BrioWuShockTube/SingleFluidResistive/main.cc
@@ -0,0 +1,72 @@
+// Serial main
+#include "simData.h"
+#include "simulation.h"
+#include "initFunc.h"
+#include "srrmhd.h"
+#include "boundaryConds.h"
+#include "SSP2.h"
+#include "saveData.h"
+#include "fluxVectorSplitting.h"
+#include "saveData.h"
+#include <omp.h>
+#include <cstring>
+
+#define ID(variable, idx, jdx, kdx) ((variable)*(data.Nx)*(data.Ny)*(data.Nz) + (idx)*(data.Ny)*(data.Nz) + (jdx)*(data.Nz) + (kdx))
+
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+
+  // Set up domain
+  int Ng(4);
+  int nx(100);
+  int ny(0);
+  int nz(0);
+  double xmin(0.0);
+  double xmax(1.0);
+  double ymin(-1.0);
+  double ymax(1.0);
+  double zmin(0.0);
+  double zmax(1.0);
+  double endTime(0.4);
+  double cfl(0.4);
+  double gamma(2.0);
+  double sigma(10);
+
+
+  Data data(nx, ny, nz, xmin, xmax, ymin, ymax, zmin, zmax, endTime,
+            cfl, Ng, gamma, sigma);
+
+  // Choose particulars of simulation
+  SRRMHD model(&data);
+
+  FVS fluxMethod(&data, &model);
+
+  Simulation sim(&data);
+
+  BrioWuSingleFluid init(&data);
+
+  Outflow bcs(&data);
+
+  SSP2 timeInt(&data, &model, &bcs, &fluxMethod);
+
+  SaveData save(&data, 1);
+
+  // Now objects have been created, set up the simulation
+  sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
+  // Time execution of programme
+  double startTime(omp_get_wtime());
+
+  // // Run until end time and save results
+  sim.evolve();
+
+  double timeTaken(omp_get_wtime() - startTime);
+
+  save.saveAll();
+  printf("\nRuntime: %.5fs\nCompleted %d iterations.\n", timeTaken, data.iters);
+
+  return 0;
+
+}
---
+++ Examples/animation.py
@@ -0,0 +1,100 @@
+"""
+    Script gathers all data from the animation data folders and stores
+    in the anim class ready to run the movie.
+"""
+
+
+from interactivePlot import InteractivePlot as Plot
+import numpy as np
+import sys
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from matplotlib import cm
+import warnings
+from copy import deepcopy
+from glob import glob
+
+warnings.filterwarnings('ignore', "No labelled objects found. ")
+
+
+
+# Change this to the relative path to the data you want to plot
+# File names must start with e.g. `primitive`
+UsualDirectory = 'Data/TimeSeries/UserDef/'
+
+class Anim(object): 
+    
+    variables = []
+    
+    def __init__(self, DataDir=None):
+        if DataDir==None:
+            self.DataDirectory = UsualDirectory
+        else:
+            self.DataDirectory = DataDir
+        with open(self.DataDirectory + 'info') as f:
+            for var in f.readlines():
+                self.variables.append(var)
+        self.final = Plot(self.DataDirectory + '../../Final/')
+        self._gatherData()
+
+    def _gatherData(self):
+        # Number of frames
+        self.vars = np.zeros([len(self.variables), self.final.c['Nx'], self.final.c['Ny'], self.final.c['Nz']])
+        self.Nframes = len(glob(self.DataDirectory + self.variables[0][:-1] + '*'))
+    
+        self.frame = []
+        self.t = []
+        # Conserved vector first
+        print("Gathering data...\nThis may take some time! Found {} frames...".format(self.Nframes))
+        for f in range(self.Nframes):
+            print("Fetching frame {}/{}".format(f+1, self.Nframes))
+            for v, var in enumerate(self.variables):
+                with open(self.DataDirectory + self.variables[v][:-1] + str(f) + '.dat') as file:
+                    for i, line in enumerate(file):
+                        if i==0:
+                            if v==0:
+                                self.t.append(float(line[line.index("t = ") + 4:]))
+                        # Get var data
+                        else:
+                            temp = line.split()
+                            for k in range(self.final.c['Nz']):
+                                self.vars[v][self.final._getXIndexFromLine(i, self.final.c['Nx'], self.final.c['Ny'])][self.final._getYIndexFromLine(i, self.final.c['Nx'], self.final.c['Ny'])][k] = float(temp[k])
+            self.frame.append(deepcopy(self.vars))
+        print("Ready!")
+    
+
+
+
+if __name__ == '__main__':
+    
+#     Get data
+    animClass = Anim()
+    
+    # Animate density
+    var = 0
+    N = animClass.final.c['Ng']
+    ##### For some reason this doesnt work when inside a function. ########
+    ##### Not a disaster atm so will leave it like this            ########
+    
+    fig = plt.figure()
+    
+    # ims is a list of lists, each row is a list of artists to draw in the
+    # current frame; here we are just animating one artist, the image, in
+    # each frame
+    ims = []
+    for i in range(animClass.Nframes):
+        im = plt.imshow(animClass.frame[i][var][N:-N, N:-N, 0], 
+                        interpolation='bicubic', 
+                        animated=True,
+                        cmap=cm.afmhot)
+        ims.append([im])
+    
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+                                    repeat_delay=10)
+    
+    plt.colorbar()
+#    plt.show()
+    ani.save('Output.gif', writer='imagemagick', fps=20)
+    
+    
+    
