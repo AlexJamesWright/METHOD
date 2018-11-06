6eea351813626cda6db6760d64993347248dc691
AlexJamesWright <a.j.wright@soton.ac.uk>
2018-11-06 14:33:59 +0000

    Need stblib for malloc and free

--- Project/Serial/Src/fluxVectorSplitting.cc
+++ Project/Serial/Src/fluxVectorSplitting.cc
@@ -1,4 +1,5 @@
 #include "fluxVectorSplitting.h"
+#include <cstdlib>
 
 // Macro for getting array index
 #define ID(variable, idx, jdx, kdx) ((variable)*(d->Nx)*(d->Ny)*(d->Nz) + (idx)*(d->Ny)*(d->Nz) + (jdx)*(d->Nz) + (kdx))
