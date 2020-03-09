4640cd7a045b4e5df4a98968be1a260f8efdb7eb
AlexJamesWright <a.j.wright@soton.ac.uk>
2020-03-09 14:27:40 +0000

    Trying to ignore paths for coveralls

--- test_helper.rb
+++ test_helper.rb
@@ -4,6 +4,9 @@ require 'coveralls'
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
    add_filter 'Examples'
+   add_filter 'PubSrc'
+   add_filter 'build'
+   add_filter 'Doxumentation'
    add_filter 'Project/Parallel'
    add
 end
