aab163fe914c19be511d05b9359df8df3232c41b
Anna Brown <annabrown@thor.local>
2020-04-06 12:49:08 +0100

    added quick script for comparing simulation vars

--- Project/ParallelMPI/Src/main.cc
+++ Project/ParallelMPI/Src/main.cc
@@ -1,33 +1,13 @@
-/**
- * @Author: Alex James Wright <alex>
- * @Date:   2019-09-30T15:33:00+01:00
- * @Email:  alex.j.wright2@gmail.com
- * @Last modified by:   alex
- * @Last modified time: 2019-10-11T13:26:33+01:00
- * @License: MIT
- */
-
-
-
 // Serial main
 #include "simData.h"
 #include "simulation.h"
 #include "initFunc.h"
 #include "srmhd.h"
-#include "srrmhd.h"
 #include "boundaryConds.h"
-#include "rkSplit2ndOrder.h"
 #include "rkSplit.h"
-#include "SSP3.h"
 #include "saveData.h"
 #include "fluxVectorSplitting.h"
-#include "REGIME.h"
-#include "hybrid.h"
-
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
+#include "saveData.h"
 #include <cstring>
 
 using namespace std;
@@ -35,29 +15,28 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
 
-  const double MU(1000);
   // Set up domain
   int Ng(4);
-  int nx(30);
-  int ny(0);
+  int nx(256);
+  int ny(256);
   int nz(0);
-  double xmin(-3);
-  double xmax(3);
-  double ymin(-1);
-  double ymax(1);
-  double zmin(-1.0);
+  double xmin(0.0);
+  double xmax(1.0);
+  double ymin(0.0);
+  double ymax(1.0);
+  double zmin(0.0);
   double zmax(1.0);
-  double endTime(7);
-  double cfl(0.8);
-  double gamma(2.0);
-  double sigma(50);
+  double endTime(3.0);
+  double cfl(0.6);
+  double gamma(4.0/3.0);
+  double sigma(10);
   double cp(1.0);
-  double mu1(-MU);
-  double mu2(MU);
-  int frameSkip(40);
+  double mu1(-100);
+  double mu2(100);
+  int frameSkip(10);
   bool output(false);
-  int safety(-1);
-
+  if (argc != 2) throw std::invalid_argument("Expected ./main seed!\n");
+  int seed(atoi(argv[1]));
 
   Data data(nx, ny, nz, xmin, xmax, ymin, ymax, zmin, zmax, endTime,
             cfl, Ng, gamma, sigma, cp, mu1, mu2, frameSkip);
@@ -67,31 +46,31 @@ int main(int argc, char *argv[]) {
 
   FVS fluxMethod(&data, &model);
 
-  REGIME modelExtension(&data, &fluxMethod);
-
   Simulation sim(&data);
 
-  CurrentSheetSingleFluid init(&data);
+  printf("Seed: %d\n", seed);
 
-  Outflow bcs(&data);
+  KHRandomInstabilitySingleFluid init(&data, 1, seed);
 
-  RKSplit timeInt(&data, &model, &bcs, &fluxMethod, &modelExtension);
+  Periodic bcs(&data);
 
-  SaveData save(&data);
+  RKSplit timeInt(&data, &model, &bcs, &fluxMethod);
+
+  SaveData save(&data, 0);
 
   // Now objects have been created, set up the simulation
   sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
-
+  
   // Time execution of programme
-  clock_t startTime(clock());
+  //double startTime(omp_get_wtime());
 
   // Run until end time and save results
-  sim.evolve(output, safety);
+  sim.evolve(output);
 
-  double timeTaken(double(clock() - startTime)/(double)CLOCKS_PER_SEC);
+  //double timeTaken(omp_get_wtime() - startTime);
 
   save.saveAll();
-  printf("\nRuntime: %.5fs\nCompleted %d iterations.\n", timeTaken, data.iters);
+  //printf("\nRuntime: %.5fs\nCompleted %d iterations.\n", timeTaken, data.iters);
 
   return 0;
 
