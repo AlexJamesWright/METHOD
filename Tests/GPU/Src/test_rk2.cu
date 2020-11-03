ccf97040b374c8a4c1a3026a3809d53f2d07b3d5
ania.brown <ania.brown@gmail.com>
2020-11-03 12:06:04 +0000

    finished converting serial GPU tests to use platformEnv object

--- Tests/GPU/Src/test_rk2.cu
+++ Tests/GPU/Src/test_rk2.cu
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "srrmhd.h"
 #include "simulation.h"
+#include "serialSaveData.h"
 #include "simData.h"
 #include "initFunc.h"
 #include "RK2.h"
@@ -15,15 +16,15 @@ TEST(RK2, RK2OutputConsistentWithSerial)
     The following was used to gather data to compare the parallel
      version with. No tests are run in the serial version of this test
   */
-
-  Data d(30, 30, 30, 0, 1, 0, 1, 0, 1, 0.004);
+  SerialEnv env(0, NULL, 1, 1, 1, 1);
+  Data d(30, 30, 30, 0, 1, 0, 1, 0, 1, 0.004, &env);
   SRRMHD model(&d);
   FVS fluxMethod(&d, &model);
-  Simulation sim(&d);
-  OTVortexSingleFluid init(&d);
   Outflow bcs(&d);
+  Simulation sim(&d, &env);
+  OTVortexSingleFluid init(&d);
   RK2 timeInt(&d, &model, &bcs, &fluxMethod);
-  SaveData save(&d);
+  SerialSaveData save(&d, &env);
   sim.set(&init, &model, &timeInt, &bcs, &fluxMethod, &save);
 
   sim.evolve();
