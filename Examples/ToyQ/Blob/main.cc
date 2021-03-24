fa2e9ef5afa27b136a02ad5068c308cc7ac14a31
Ian Hawke <I.Hawke@soton.ac.uk>
2021-03-24 10:53:31 +0000

    Mostly comments about stability issues.

--- Examples/ToyQ/Blob/main.cc
+++ Examples/ToyQ/Blob/main.cc
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
 
   // Set up domain
   int Ng(4);
-  int nx(256);
+  int nx(2048);
   int ny(0);
   int nz(0);
   double xmin(0.0);
@@ -28,14 +28,19 @@ int main(int argc, char *argv[]) {
   double ymax(1.0);
   double zmin(0.0);
   double zmax(1.0);
-  double endTime(4.0);
+  double endTime(100.0);
   double cfl(0.4);
   // double gamma(0.001);
   // double sigma(0.001);
   // These parameters work with IMEX SSP2; given that tau_q << dt,
   // we should not expect them to work with the explicit solver, and indeed
   // it fails very quickly.
-  double gamma(0.001);
+  // Note it's the ratio that matters to it being stable with IMEX.
+  // Need gamma/sigma < 10 or so at moderate resolution, but at higher resolution
+  // it seems to fall over regardless of the values.
+  // It seems to be the case that above 1024 we need to really reduce gamma - ratio
+  // remains fine. Of course, reducing gamma means increasing endTime to see anything.
+  double gamma(0.00001);
   double sigma(0.000001);
   double cp(1.0);
   double mu1(-100);
@@ -57,11 +62,13 @@ int main(int argc, char *argv[]) {
 
   FVS fluxMethod(&data, &weno, &model);
 
-  Outflow bcs(&data);
+  // Outflow bcs(&data);
+  Periodic bcs(&data);
 
   Simulation sim(&data, &env);
 
   BlobToyQ init(&data);
+  // Blob2dToyQ init(&data);
 
   // RKSplit timeInt(&data, &model, &bcs, &fluxMethod);
   // BackwardsRK2 timeInt(&data, &model, &bcs, &fluxMethod);
@@ -77,7 +84,7 @@ int main(int argc, char *argv[]) {
 
   for (int n(0); n<nreports; n++) {
     data.endTime = (n+1)*endTime/(nreports);
-    SerialSaveDataHDF5 save_in_loop(&data, &env, "data_serial"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
+    SerialSaveDataHDF5 save_in_loop(&data, &env, "1d/data_serial"+std::to_string(n+1), SerialSaveDataHDF5::OUTPUT_ALL);
     sim.evolve(output);
     save_in_loop.saveAll();
   }
